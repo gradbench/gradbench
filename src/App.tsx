cbf75d0d234df0d76afd775615ec9f99114c2e1e
Sam Estep <sam@samestep.com>
2024-05-29 13:11:15 -0400

    Add GitHub link

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import { useState } from "react";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>GradBench</h1>
+      <p>
+        <a href="https://github.com/samestep/gradbench">GitHub</a>
       </p>
     </>
   );
-}
+};
 
 export default App;
