86ee065e1f6d362b1636e9a5a313ba07636f4533
Sam Estep <sam@samestep.com>
2024-05-29 13:16:43 -0400

    Make website (#3)

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <>
+      <h1>GradBench</h1>
+      <p>
+        <a href="https://github.com/samestep/gradbench">GitHub</a>
+      </p>
+    </>
+  );
+};
+
+export default App;
