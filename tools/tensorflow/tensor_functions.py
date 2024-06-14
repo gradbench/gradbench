5e21c454f7e580af9cd7de69da529ebefe394356
Maggie Hollis <89551084+maggiehollis@users.noreply.github.com>
2024-06-14 16:47:04 -0400

    TensorFlow Implementation (#6)

---
+++ tools/tensorflow/tensor_functions.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+def square(x):
+    return x*x
+
+def double(x):
+    with tf.GradientTape() as tape:
+        tape.watch(x)
+        y = square(x)
+                
+    grad = tape.gradient(y,x)
+    return grad
