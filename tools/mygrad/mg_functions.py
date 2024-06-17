bd32e1089dd7a287e6f24734e5afde66e0c3a501
maggiehollis <mhollis@smith.edu>
2024-06-17 15:14:32 -0400

    Implemented MyGrad

---
+++ tools/mygrad/mg_functions.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def square(x):
+    return x * x
+
+def double(x):
+    y = square(x)
+    y.backward()
+    return x.grad
---
+++ tools/mygrad/mg_run.py
@@ -0,0 +1,35 @@
+import json
+import sys
+import time
+from importlib import import_module
+
+import mygrad as mg
+import numpy as np
+
+
+def resolve(name):
+    functions = import_module("mg_functions")
+    return getattr(functions, name)
+
+
+def tensor(x):
+    return mg.tensor(x,dtype=np.float64)
+
+
+def run(params):
+    func = resolve(params["name"])
+    vals = [tensor(arg["value"]) for arg in params["arguments"]]
+    start = time.perf_counter_ns()
+    ret = func(*vals)
+    end = time.perf_counter_ns()
+    return {"return": ret.item(), "nanoseconds": end - start}
+
+
+def main():
+    cfg = json.load(sys.stdin)
+    outputs = [run(params) for params in cfg["inputs"]]
+    print(json.dumps({"outputs": outputs}))
+
+
+if __name__ == "__main__":
+    main()
