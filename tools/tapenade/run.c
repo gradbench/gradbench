c18396cde06aa62acf41aa63da9af67ccece9e80
maggiehollis <mhollis@smith.edu>
2024-06-18 16:31:44 -0400

    Add JSON

--- tools/tapenade/run.c
+++ tools/tapenade/run.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void square(double x, double *y);
 void square_b(double x, double *xb, double *y, double *yb);
 
-int main() {
-    double x = 3.0;    // Input from JSON
-    
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("Usage: %s <value of x>\n", argv[0]);
+        return 1;  // Exit if no input is provided
+    }
+    double x = atof(argv[1]);
+    char* func = argv[2];
+
     double y;          // Function result
     double xb = 0.0;   // This will hold the derivative dy/dx
     double yb = 1.0;   // Set to 1 to compute the derivative
 
-    square(x, &y);
-    square_b(x, &xb, &y, &yb);
-
-    // Print the results
-    printf("y = %f\n", y);
-    printf("dy/dx = %f\n", xb);
-
+    if (strcmp(func,"square") == 0){
+        square(x, &y);
+        printf("%.1f", y);
+    }
+    else{
+        square(x, &y);
+        square_b(x, &xb, &y, &yb);
+        printf("%.1f", xb);
+    }
     return 0;
 }
