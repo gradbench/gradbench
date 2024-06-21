b37158f18ec796385a919fbe55a1f1a643273824
Sam Estep <sam@samestep.com>
2024-06-21 13:07:44 -0400

    Parse JSON

--- tools/scilean/Main.lean
+++ tools/scilean/Main.lean
@@ -1,2 +1,36 @@
-def main : IO Unit :=
-  IO.println s!"Hello, world!"
+import Lean
+
+structure Argument where
+  value: Float
+deriving Lean.FromJson, Repr
+
+instance : ToString Argument where
+  toString : Argument -> String
+    | { value } => s!"\{ value := {value} }"
+
+structure Input where
+  name: String
+  arguments: List Argument
+deriving Lean.FromJson, Repr
+
+instance : ToString Input where
+  toString : Input -> String
+    | { name, arguments } => s!"\{ name := {name}, arguments := {arguments} }"
+
+partial def readToEnd (stream : IO.FS.Stream) : IO String := do
+  let rec loop (s : String) := do
+    let line ← stream.getLine
+    if line.isEmpty then
+      return s
+    else
+      loop (s ++ line)
+  loop ""
+
+def main : IO Unit := do
+  let stdin <- IO.getStdin
+  let s <- readToEnd stdin
+  let result: Except String (List Input) := do
+    let cfg <- Lean.Json.parse s
+    let inputs <- Lean.Json.getObjVal? cfg "inputs"
+    Lean.FromJson.fromJson? inputs
+  IO.println s!"{result}"
