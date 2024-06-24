efeb3918ec27d9ab06f9dc70700db159b95b1c21
lecopivo <skrivantomas@seznam.cz>
2024-06-24 16:26:00 -0400

    run AD benchmark in IO

--- tools/scilean/lakefile.lean
+++ tools/scilean/lakefile.lean
@@ -6,5 +6,6 @@ package «gradbench»
 @[default_target]
 lean_exe «gradbench» where
   root := `Main
+  supportInterpreter := true
 
 require scilean from git "https://github.com/lecopivo/SciLean.git" @ "22d53b2f4e3db2a172e71da6eb9c916e62655744"
