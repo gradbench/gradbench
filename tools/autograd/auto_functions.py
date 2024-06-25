9c1ed1e1278a6a76c4638e366e0a01008f64a542
maggiehollis <mhollis@smith.edu>
2024-06-25 16:49:08 -0400

    add autograd

---
+++ tools/autograd/auto_functions.py
@@ -0,0 +1,8 @@
+from autograd import grad
+
+def square(x):
+    return x * x
+
+def double(x):
+    y = grad(square)
+    return y(x)
---
+++ tools/autograd/auto_run.py
@@ -0,0 +1,25 @@
+import json
+import sys
+import time
+from importlib import import_module
+
+def resolve(name):
+    functions = import_module("auto_functions")
+    return getattr(functions, name)
+
+def run(params):
+    func = resolve(params["name"])
+    vals = [arg["value"]*1.0 for arg in params["arguments"]]
+    start = time.perf_counter_ns()
+    ret = func(*vals)
+    end = time.perf_counter_ns()
+    return {"return": ret, "nanoseconds": end - start}
+
+def main():
+    cfg = json.load(sys.stdin)
+    outputs = [run(params) for params in cfg["inputs"]]
+    print(json.dumps({"outputs": outputs}))
+
+
+if __name__ == "__main__":
+    main()
