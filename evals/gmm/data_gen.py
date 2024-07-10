4b98240a100bef49cf47e6b466864b9073306d07
maggiehollis <mhollis@smith.edu>
2024-07-10 13:16:52 -0400

    Generate GMM data in docker image

---
+++ evals/gmm/data_gen.py
@@ -0,0 +1,145 @@
+# Copyright (c) Microsoft Corporation.
+
+# MIT License
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# https://github.com/microsoft/ADBench/blob/38cb7931303a830c3700ca36ba9520868327ac87/data/gmm/gmm-data-gen.py
+
+import os
+import sys
+
+import numpy as np
+
+
+# function printing to stderr
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+
+def get_points_dir_name(n):
+    if n == 1000:
+        return "1k"
+    if n == 10000:
+        return "10k"
+    if n == 2500000:
+        return "2.5M"
+    raise ValueError("Undefined number of points: {n}")
+
+
+def replicate_point(n):
+    if n == 1000:
+        return False
+    if n == 10000:
+        return False
+    if n == 2500000:
+        return True
+    raise ValueError("Undefined number of points: {n}")
+
+
+def generate(data_uniform, data_normal, D, k, n):
+
+    gamma = 1.0
+    m = 0
+
+    view_uniform = data_uniform[:]
+    view_normal = data_normal[:]
+
+    filename = f"gmm_d{D}_K{k}_N{n}.txt"
+
+    # write data to file
+    with open(os.path.join(os.getcwd(), filename), "w") as outfile:
+
+        outfile.write(f"{D} {k} {n}\n")
+
+        # alpha
+        for i in range(k):
+            outfile.write(f"{view_normal[0]:.6f}\n")
+            view_normal = view_normal[1:]
+
+        # mu
+        for i in range(k):
+            for j in range(D):
+                outfile.write(f"{view_uniform[0]:.6f} ")
+                view_uniform = view_uniform[1:]
+            outfile.write("\n")
+
+        # q
+        for i in range(k):
+            for j in range(D + D * (D - 1) // 2):
+                outfile.write(f"{view_normal[0]:.6f} ")
+                view_normal = view_normal[1:]
+            outfile.write("\n")
+
+        # x
+        if replicate_point(n):
+            for j in range(D):
+                outfile.write(f"{view_normal[0]:.6f} ")
+                view_normal = view_normal[1:]
+            outfile.write("\n")
+        else:
+            for i in range(n):
+                for j in range(D):
+                    outfile.write(f"{view_normal[0]:.6f} ")
+                    view_normal = view_normal[1:]
+                outfile.write("\n")
+
+        outfile.write(f"{gamma:.6f} {m} \n")
+
+
+def generator(d, K, N):
+
+    # uniform distribution parameters
+    low = 0
+    high = 1
+    amount_of_uniform_numbers = K[-1] * d
+    data_uniform = np.random.uniform(low, high, amount_of_uniform_numbers)
+
+    # normal distribution parameters
+    mean = 0
+    sigma = 1
+    amount_of_normal_numbers = K[-1] * (1 + d + d * (d - 1) // 2) + N[-2] * d
+    data_normal = np.random.normal(mean, sigma, amount_of_normal_numbers)
+
+    # only runs on 1000 for now
+    # for n in N:
+    for k in K:
+        generate(data_uniform, data_normal, d, k, N[0])
+
+
+def main():
+    try:
+
+        d = 2
+        K = [5, 10, 25, 50, 100, 200]
+        N = [1000, 10000, 2500000]
+
+        # generate GMM models with d dimensions
+        generator(d, K, N)
+
+    except RuntimeError as ex:
+        eprint("Runtime exception caught: ", ex)
+    except Exception as ex:
+        eprint("An exception caught: ", ex)
+
+    return 0
+
+
+if __name__ == "__main__":
+    main(sys.argv[:])
