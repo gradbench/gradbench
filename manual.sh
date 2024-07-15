1de541006320c8f449cef023a2c6b9e9d53ffcaf
Sam Estep <sam@samestep.com>
2024-07-15 00:15:57 -0400

    Mark the nightly images as `latest` (#39)

--- manual.sh
+++ manual.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 docker build --platform linux/amd64,linux/arm64 "docker/$1" --tag "ghcr.io/gradbench/$1"
 tag=$(docker run --rm "ghcr.io/gradbench/$1")
-docker image tag "ghcr.io/gradbench/$1" "ghcr.io/gradbench/$1:$tag"
+docker tag "ghcr.io/gradbench/$1" "ghcr.io/gradbench/$1:$tag"
 docker push "ghcr.io/gradbench/$1:$tag"
