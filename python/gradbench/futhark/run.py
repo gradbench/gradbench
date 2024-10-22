3c22777ec0b4e1955a7eb3cff3667b92b418ab54
Troels Henriksen <athas@sigkill.dk>
2024-10-22 20:12:28 +0200

    Separate files.

--- python/gradbench/futhark/run.py
+++ python/gradbench/futhark/run.py
@@ -5,9 +5,9 @@ import os
 import json
 import sys
 import time
-import futhark_data
 import futhark_server
 import subprocess
+from importlib import import_module
 
 def server_prog_source(prog):
     return os.path.join(os.path.dirname(__file__), prog + ".fut")
@@ -15,77 +15,28 @@ def server_prog_source(prog):
 def server_prog(prog):
     return os.path.join(os.path.dirname(__file__), prog)
 
-def run_hello(params):
-    with futhark_server.Server(server_prog('hello')) as server:
-        server.put_value("input", np.float64(params["input"]))
-        start=time.time()
-        server.cmd_call(params["name"], "output", "input")
-        output = server.get_value('output')
-        end=time.time()
-        return {"output": str(output), "nanoseconds": {"evaluate": end - start}}
+def resolve(module, name):
+    functions = import_module(module)
+    return getattr(functions, name)
 
-def run_gmm(params):
-    with futhark_server.Server(server_prog('gmm')) as server:
-        d = params["input"]["d"]
-        k = params["input"]["k"]
-        n = params["input"]["n"]
-        alpha = np.array(params["input"]["alpha"], dtype=np.float64)
-        means = np.array(params["input"]["means"], dtype=np.float64)
-        icf = np.array(params["input"]["icf"], dtype=np.float64)
-        x = np.array(params["input"]["x"], dtype=np.float64)
-        gamma = np.float64(params["input"]["gamma"])
-        m = np.int64(params["input"]["m"])
-        server.put_value('alpha', alpha)
-        server.put_value('means', means)
-        server.put_value('icf', icf)
-        server.put_value('x', x)
-        server.put_value('gamma', gamma)
-        server.put_value('m', m)
-        if params["name"] == "calculate_objectiveGMM":
-            start=time.time()
-            server.cmd_call('calculate_objective',
-                            'output',
-                            'alpha',
-                            'means',
-                            'icf',
-                            'x',
-                            'gamma',
-                            'm')
-            end=time.time()
-            output = server.get_value('output')
-            server.cmd_free('output')
-        else:
-            start=time.time()
-            server.cmd_call('calculate_jacobian',
-                            'output0',
-                            'output1',
-                            'output2',
-                            'alpha',
-                            'means',
-                            'icf',
-                            'x',
-                            'gamma',
-                            'm')
-            end=time.time()
-            output = (server.get_value('output0'),
-                      server.get_value('output1'),
-                      server.get_value('output2'))
-        return {"output": str(output), "nanoseconds": {"evaluate": end - start}}
-
-modules = { 'hello': run_hello,
-             'gmm': run_gmm
-           }
+def run(params):
+    with futhark_server.Server(server_prog(params["module"])) as server:
+        prepare = resolve(params["module"], "prepare")
+        run = resolve(params["module"], params["name"])
+        prepare(server, params)
+        start = time.perf_counter_ns()
+        ret = run(server)
+        end = time.perf_counter_ns()
+        return {"output": str(ret), "nanoseconds": {"evaluate": end - start}}
 
 FUTHARK_BACKEND='c'
 
 def main():
-    server = None
-    run = None
     for line in sys.stdin:
         message = json.loads(line)
         response = {}
         if message["kind"] == "evaluate":
-            response = modules[message["module"]](message)
+            response = run(message)
         elif message["kind"] == "define":
             c = subprocess.run(['futhark',
                                 FUTHARK_BACKEND,
