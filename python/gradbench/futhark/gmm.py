a81b235a27180dc09e267d8b6fe26621d0900168
Troels Henriksen <athas@sigkill.dk>
2024-10-22 22:54:39 +0200

    Futhark implementations of benchmarks (#91)

---
+++ python/gradbench/futhark/gmm.py
@@ -0,0 +1,39 @@
+import numpy as np
+import os
+import futhark_server
+
+
+def prepare(server, params):
+    server.put_value("alpha", np.array(params["input"]["alpha"], dtype=np.float64))
+    server.put_value("means", np.array(params["input"]["means"], dtype=np.float64))
+    server.put_value("icf", np.array(params["input"]["icf"], dtype=np.float64))
+    server.put_value("x", np.array(params["input"]["x"], dtype=np.float64))
+    server.put_value("gamma", np.float64(params["input"]["gamma"]))
+    server.put_value("m", np.int64(params["input"]["m"]))
+
+
+def calculate_objectiveGMM(server):
+    server.cmd_call(
+        "calculate_objective", "output", "alpha", "means", "icf", "x", "gamma", "m"
+    )
+    return server.get_value("output")
+
+
+def calculate_jacobianGMM(server):
+    server.cmd_call(
+        "calculate_jacobian",
+        "output0",
+        "output1",
+        "output2",
+        "alpha",
+        "means",
+        "icf",
+        "x",
+        "gamma",
+        "m",
+    )
+    return (
+        server.get_value("output0"),
+        server.get_value("output1"),
+        server.get_value("output2"),
+    )
---
+++ python/gradbench/futhark/run.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import os
+import json
+import sys
+import time
+import futhark_server
+import subprocess
+from importlib import import_module
+
+
+def server_prog_source(prog):
+    return os.path.join(os.path.dirname(__file__), prog + ".fut")
+
+
+def server_prog(prog):
+    return os.path.join(os.path.dirname(__file__), prog)
+
+
+def resolve(module, name):
+    functions = import_module(module)
+    return getattr(functions, name)
+
+
+def run(params):
+    with futhark_server.Server(server_prog(params["module"])) as server:
+        prepare = resolve(params["module"], "prepare")
+        run = resolve(params["module"], params["name"])
+        prepare(server, params)
+        start = time.perf_counter_ns()
+        ret = run(server)
+        end = time.perf_counter_ns()
+        return {"output": str(ret), "nanoseconds": {"evaluate": end - start}}
+
+
+FUTHARK_BACKEND = "c"
+
+
+def main():
+    for line in sys.stdin:
+        message = json.loads(line)
+        response = {}
+        if message["kind"] == "evaluate":
+            response = run(message)
+        elif message["kind"] == "define":
+            c = subprocess.run(
+                [
+                    "futhark",
+                    FUTHARK_BACKEND,
+                    "--server",
+                    server_prog_source(message["module"]),
+                ]
+            )
+            response["success"] = c.returncode == 0
+        print(json.dumps({"id": message["id"]} | response), flush=True)
+
+
+if __name__ == "__main__":
+    main()
