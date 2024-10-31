d2be3079dac9331fb6ef529e2769687216d56af4
maggiehollis <mhollis@smith.edu>
2024-10-31 11:58:45 -0400

    Gmm for Autograd

---
+++ python/gradbench/autograd/gmm.py
@@ -0,0 +1,100 @@
+# Copyright (c) Microsoft Corporation.
+
+# MIT License
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# https://github.com/microsoft/ADBench/blob/38cb7931303a830c3700ca36ba9520868327ac87/tools/Autograd/Autograd_gmm_full.py
+
+"""
+Changes Made:
+- Used pieces of original but adjusted the format to follow a class base structure similar to pytorch/gmm.py
+"""
+
+
+import autograd.numpy as np
+from autograd import value_and_grad
+
+from gradbench.adbench.itest import ITest
+from gradbench.autograd.gmm_objective import gmm_objective
+from gradbench.wrap_module import wrap
+
+
+class AutogradGMM(ITest):
+    def prepare(self, inputs):
+        self.inputs = inputs
+        self.objective = None
+        self.gradient = None
+
+    def output(self):
+        return
+
+    def calculate_objective(self, times):
+        for i in range(times):
+            self.objective = gmm_objective(
+                self.inputs[0],
+                self.inputs[1],
+                self.inputs[2],
+                self.inputs[3],
+                self.inputs[4],
+                self.inputs[5],
+            )
+
+    def calculate_jacobian(self, times):
+        grad_gmm_objective_wrapper = value_and_grad(gmm_objective_wrapper)
+        for i in range(times):
+            _, self.gradient = grad_gmm_objective_wrapper(
+                self.inputs[0],
+                self.inputs[1],
+                self.inputs[2],
+                self.inputs[3],
+                self.inputs[4],
+                self.inputs[5],
+            )
+
+
+def gmm_objective_wrapper(alphas, means, icf, x, wishart_gamma, wishart_m):
+    return gmm_objective(alphas, means, icf, x, wishart_gamma, wishart_m)
+
+
+def prepare_input(input):
+    return (
+        np.array(input["alpha"]),
+        np.array(input["means"]),
+        np.array(input["icf"]),
+        np.array(input["x"]),
+        np.array(input["gamma"]),
+        np.array(input["m"]),
+    )
+
+
+@wrap(prepare_input, lambda x: x.tolist())
+def calculate_jacobianGMM(input):
+    py = AutogradGMM()
+    py.prepare(input)
+    py.calculate_jacobian(1)
+    return py.gradient
+
+
+@wrap(prepare_input, lambda x: x.tolist())
+def calculate_objectiveGMM(input):
+    py = AutogradGMM()
+    py.prepare(input)
+    py.calculate_objective(1)
+    return py.objective
