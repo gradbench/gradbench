8bde67482f77b58fc05f105babd7b45d8f34209b
Troels Henriksen <athas@sigkill.dk>
2024-11-22 17:15:59 +0100

    Port ht from ADBench.
    ADBench is a bit inconsistent about whether this benchmark is
    abbreviated "HT" (for Hand Tracking) or "HAND". I have chosen the
    former, as it is more consistent with the other names of ADBench
    benchmarks.
    
    Currently two tools implement this eval: PyTorch (implementation also
    from ADBench) and Futhark. They validate against each other.
    
    The run.py file for ht is currently hardcoded to only consider two
    datasets for each of the four "classes", because the larger datasets
    are extremely time consuming to run with PyTorch.

---
+++ python/gradbench/evals/ht/io.py
@@ -0,0 +1,133 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+import os
+
+import os
+import numpy as np
+
+from gradbench.adbench.ht_data import HandData, HandModel, HandInput
+
+DELIM = ":"
+
+
+def parse_floats(arr):
+    """Parses enumerable as float.
+
+    Args:
+        arr (enumerable): input data that can be parsed to floats.
+
+    Returns:
+        (List[float]): parsed data.
+    """
+
+    return [float(x) for x in arr]
+
+
+def load_model(path):
+    """Loads HandModel from the given file.
+
+    Args:
+        path(str): path to a directory with input files.
+
+    Returns:
+        (HandModel): hand trcking model.
+    """
+
+    # Read in triangle info.
+    triangles = np.loadtxt(os.path.join(path, "triangles.txt"), int, delimiter=DELIM)
+
+    # Process bones file.
+    bones_path = os.path.join(path, "bones.txt")
+
+    # Grab bone names.
+    bone_names = tuple(line.split(DELIM)[0] for line in open(bones_path))
+
+    # Grab bone parent indices.
+    parents = np.loadtxt(bones_path, int, usecols=[1], delimiter=DELIM).flatten()
+
+    # Grab relative transforms.
+    relative_transforms = np.loadtxt(
+        bones_path, usecols=range(2, 2 + 16), delimiter=DELIM
+    ).reshape(len(parents), 4, 4)
+
+    vertices_path = os.path.join(path, "vertices.txt")
+    n_bones = len(bone_names)
+
+    # Find number of vertices.
+    with open(vertices_path) as handle:
+        n_verts = len(handle.readlines())
+
+    # Read in vertex info.
+    positions = np.zeros((n_verts, 3))
+    weights = np.zeros((n_verts, n_bones))
+
+    with open(vertices_path) as handle:
+        for i_vert, line in enumerate(handle):
+            atoms = line.split(DELIM)
+            positions[i_vert] = parse_floats(atoms[:3])
+
+            for i in range(int(atoms[8])):
+                i_bone = int(atoms[9 + i * 2])
+                weights[i_vert, i_bone] = float(atoms[9 + i * 2 + 1])
+
+    # Grab absolute invers transforms.
+    inverse_absolute_transforms = np.loadtxt(
+        bones_path, usecols=range(2 + 16, 2 + 16 + 16), delimiter=DELIM
+    ).reshape(len(parents), 4, 4)
+
+    n_vertices = positions.shape[0]
+    homogeneous_base_positions = np.ones((n_vertices, 4))
+    homogeneous_base_positions[:, :3] = positions
+
+    result = HandModel(
+        n_bones,
+        bone_names,
+        parents,
+        relative_transforms,
+        inverse_absolute_transforms,
+        homogeneous_base_positions,
+        weights,
+        triangles,
+        False,  # WARNING: not exactly understand where such info comes from
+    )
+
+    return result
+
+
+def read_hand_instance(model_dir, fn, read_us):
+    """Reads input data for hand tracking objective.
+
+    Args:
+        model_dir (str): path to the directory contatins model data files.
+        fn (str): name of the file contains additional data for objective.
+        read_us (bool): if True then complicated scheme is used.
+
+    Returns:
+        (HandInput): input data for hand objective test class.
+    """
+
+    model = load_model(model_dir)
+
+    fid = open(fn, "r")
+    line = fid.readline()
+    line = line.split()
+    npts = int(line[0])
+    ntheta = int(line[1])
+
+    lines = [fid.readline().split() for _ in range(npts)]
+    correspondences = np.array([int(line[0]) for line in lines])
+    points = np.array([parse_floats(line[1:]) for line in lines])
+
+    if read_us:
+        us = np.array([parse_floats(fid.readline().split()) for _ in range(npts)])
+
+    params = np.array([float(fid.readline()) for _ in range(ntheta)])
+    fid.close()
+
+    data = HandData(model, correspondences, points)
+
+    if read_us:
+        return HandInput(params, data, us)
+    else:
+        return HandInput(params, data)
---
+++ python/gradbench/evals/ht/run.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+from typing import Any
+import numpy as np
+
+import gradbench.pytorch.ht as golden
+from gradbench.evaluation import SingleModuleValidatedEvaluation, assertion
+from gradbench.wrap_module import Functions
+from gradbench.evals.ht import io
+
+
+def check(name: str, input: Any, output: Any) -> None:
+    func: Functions = getattr(golden, name)
+    expected = func.unwrap(func(func.prepare(input)))
+    assert np.all(np.isclose(expected, output))
+
+
+def main():
+    e = SingleModuleValidatedEvaluation(module="ht", validator=assertion(check))
+    if e.define().success:
+        # NOTE: data files are taken directly from ADBench.
+        simple_small = Path(__file__).parent / "data" / "simple_small"
+        simple_big = Path(__file__).parent / "data" / "simple_big"
+        complicated_small = Path(__file__).parent / "data" / "complicated_small"
+        complicated_big = Path(__file__).parent / "data" / "complicated_big"
+
+        def evals(data_dir, complicated):
+            # Shrink the range because some of the larger datasets take an
+            # excessive amount of time with PyTorch.
+            for i in range(1, 13)[:2]:
+                fn = next(data_dir.glob(f"hand{i}_*.txt"), None)
+                model_dir = data_dir / "model"
+                input = io.read_hand_instance(model_dir, fn, complicated).to_dict()
+                e.evaluate(name="calculate_objectiveHT", input=input)
+                e.evaluate(name="calculate_jacobianHT", input=input)
+
+        evals(simple_small, False)
+        evals(simple_big, False)
+        evals(complicated_small, True)
+        evals(complicated_big, True)
+
+    e.end()
+
+
+if __name__ == "__main__":
+    main()
