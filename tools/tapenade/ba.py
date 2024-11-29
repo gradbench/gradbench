a7099e6dfde13136a075b68f7812ac8b579977f5
Troels Henriksen <athas@sigkill.dk>
2024-11-30 00:26:05 +0100

    Reformat.

--- tools/tapenade/ba.py
+++ tools/tapenade/ba.py
@@ -3,18 +3,25 @@ import tempfile
 import json
 from os import listdir
 
+
 def compile():
     # Nothing to do here. We assume everything is precompiled.
     return True
 
+
 def calculate_objectiveBA(input):
-    with tempfile.NamedTemporaryFile('w') as tmp:
+    with tempfile.NamedTemporaryFile("w") as tmp:
         json.dump(input, tmp)
         tmp.flush()
-        return subprocess.run(["./run_ba", tmp.name, "F"], text=True, capture_output=True)
+        return subprocess.run(
+            ["./run_ba", tmp.name, "F"], text=True, capture_output=True
+        )
+
 
 def calculate_jacobianBA(input):
-    with tempfile.NamedTemporaryFile('w') as tmp:
+    with tempfile.NamedTemporaryFile("w") as tmp:
         json.dump(input, tmp)
         tmp.flush()
-        return subprocess.run(["./run_ba", tmp.name, "J"], text=True, capture_output=True)
+        return subprocess.run(
+            ["./run_ba", tmp.name, "J"], text=True, capture_output=True
+        )
