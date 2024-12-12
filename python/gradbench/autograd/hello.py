7533e9b913bba243f6eb2deb8d5ab863cfcb4714
Sam Estep <sam@samestep.com>
2024-12-12 15:34:53 -0500

    Replace `id` with `lambda x: x`

--- python/gradbench/autograd/hello.py
+++ python/gradbench/autograd/hello.py
@@ -3,12 +3,12 @@ from autograd import grad
 from gradbench import wrap
 
 
-@wrap.function(pre=lambda x: x * 1.0, post=id)
+@wrap.function(pre=lambda x: x * 1.0, post=lambda x: x)
 def square(x):
     return x * x
 
 
-@wrap.function(pre=lambda x: x * 1.0, post=id)
+@wrap.function(pre=lambda x: x * 1.0, post=lambda x: x)
 def double(x):
     gradient = grad(square)
     return gradient(x)
