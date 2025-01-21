db6233a6c46b93132adfe32d3fae02e2c53f663b
Troels Henriksen <athas@sigkill.dk>
2025-01-21 19:12:21 +0100

    Implement Enzyme-HT.

---
+++ tools/enzyme/EnzymeHT.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "adbench/shared/ITest.h"
+#include "adbench/shared/HTData.h"
+
+class EnzymeHand : public ITest<HandInput, HandOutput> {
+  HandInput _input;
+  HandOutput _output;
+  bool _complicated = false;
+
+public:
+  void prepare(HandInput&& input) override;
+
+  void calculate_objective(int times) override;
+  void calculate_jacobian(int times) override;
+  HandOutput output() override;
+
+  ~EnzymeHand() = default;
+};
