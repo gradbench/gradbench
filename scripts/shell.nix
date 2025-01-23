de8ef1abe40fe98660d31520d5d7835f3c0daf71
Troels Henriksen <athas@sigkill.dk>
2025-01-23 19:41:46 +0100

    Fix shell.nix.

--- scripts/shell.nix
+++ scripts/shell.nix
@@ -47,7 +47,8 @@ pkgs.stdenv.mkDerivation {
      pkgs.rustfmt
      pkgs.futhark
      pkgs.enzyme
+     pkgs.adolc
      pkgs.llvmPackages_19.lld
-     pkgs.llvmPackages_19.clang++
+     pkgs.llvmPackages_19.clang
     ];
 }
