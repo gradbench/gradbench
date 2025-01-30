45724b63ad2aeeb6084a97feaa7a9e8cbd5c8e25
Troels Henriksen <athas@sigkill.dk>
2025-01-30 12:31:48 +0100

    hello is special.

--- tools/enzyme/hello.py
+++ tools/enzyme/hello.py
@@ -1,1 +1,29 @@
-../../python/gradbench/gradbench/cpp_main.py
+import json
+import subprocess
+import tempfile
+from os import listdir
+
+
+def compile():
+    c = subprocess.run(
+        ["make", "-C", "tools/enzyme", "-B", "-j", "run_hello"], stdout=2
+    )
+    return c.returncode == 0
+
+
+def square(input):
+    with tempfile.NamedTemporaryFile("w") as tmp:
+        json.dump(input, tmp)
+        tmp.flush()
+        return subprocess.run(
+            ["tools/enzyme/run_hello", tmp.name, "F"], text=True, capture_output=True
+        )
+
+
+def double(input):
+    with tempfile.NamedTemporaryFile("w") as tmp:
+        json.dump(input, tmp)
+        tmp.flush()
+        return subprocess.run(
+            ["tools/enzyme/run_hello", tmp.name, "J"], text=True, capture_output=True
+        )
