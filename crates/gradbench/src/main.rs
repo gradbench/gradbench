cae76af189655fa3faba1490bce9fb27baebacc7
Sam Estep <sam@samestep.com>
2025-02-12 17:21:42 -0500

    Delete `tools/diffsharp` (#239)

--- crates/gradbench/src/main.rs
+++ crates/gradbench/src/main.rs
@@ -1022,7 +1022,6 @@ fn cli_result() -> Result<(), ExitCode> {
                     evals.sort();
                     github_output("eval", evals)?;
                     let mut tools = ls("tools")?;
-                    tools.retain(|t| t != "diffsharp"); // Flaky.
                     tools.sort();
                     github_output("tool", &tools)?;
                     Ok(())
--- tools/diffsharp/hello.fs
+++
@@ -1,7 +0,0 @@
-module hello
-
-open DiffSharp
-
-let square x = x * x
-
-let double x = (dsharp.grad square) x
