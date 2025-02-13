26b034e2d6b21b4a0fc158c073e0f3f2f37bd8a1
Troels Henriksen <athas@sigkill.dk>
2025-02-13 18:30:40 +0100

    Fix various kmeans problems (#243)

--- python/gradbench/gradbench/evals/kmeans/run.py
+++ python/gradbench/gradbench/evals/kmeans/run.py
@@ -20,7 +20,7 @@ def check(function: str, input: Any, output: Any) -> None:
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-k", nargs="+", type=int, default=[10, 100, 1000])
-    parser.add_argument("-n", nargs="+", type=int, default=[1000, 10000, 100000])
+    parser.add_argument("-n", nargs="+", type=int, default=[1000, 10000])
     parser.add_argument("-d", nargs="+", type=int, default=[8, 16])
     parser.add_argument("--runs", type=int, default=1)
     args = parser.parse_args()
@@ -28,24 +28,29 @@ def main():
     e = SingleModuleValidatedEval(module="kmeans", validator=mismatch(check))
     e.start()
     if e.define().success:
-        for k in args.k:
-            for n in args.n:
-                for d in args.d:
-                    points = np.random.rand(n, d).tolist()
-                    # This guarantees each cluster is nonempty.
-                    centroids = points[-k:]
-                    input = {"points": points, "centroids": centroids}
-                    e.evaluate(
-                        function="cost",
-                        input=input | {"runs": args.runs},
-                        description=f"k={k},n={n},d={d}",
-                    )
-                    e.evaluate(
-                        function="dir",
-                        input=input | {"runs": args.runs},
-                        description=f"k={k},n={n},d={d}",
-                    )
+        combinations = sorted(
+            [(k, n, d) for k in args.k for n in args.n for d in args.d],
+            key=lambda v: v[0] * v[1] * v[2],
+        )
+        for k, n, d in combinations:
+            points = np.random.rand(n, d).tolist()
+            # This guarantees each cluster is nonempty.
+            centroids = points[-k:]
+            input = {"points": points, "centroids": centroids}
+            e.evaluate(
+                function="cost",
+                input=input | {"runs": args.runs},
+                description=f"k={k},n={n},d={d}",
+            )
+            e.evaluate(
+                function="dir",
+                input=input | {"runs": args.runs},
+                description=f"k={k},n={n},d={d}",
+            )
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except (EOFError, BrokenPipeError):
+        pass
