b72156867af7489b1bb5fb5b521c597d5acead41
Troels Henriksen <athas@sigkill.dk>
2025-02-16 12:32:08 +0100

    Remove more unused code.

--- cpp/adbench/shared/utils.h
+++ cpp/adbench/shared/utils.h
@@ -16,11 +16,6 @@
 
 #include "defs.h"
 
-#ifdef DO_EIGEN
-#include "hand_eigen_model.h"
-#endif
-
-
 class HandModelLightMatrix
 {
 public:
@@ -42,43 +37,6 @@ public:
     LightMatrix<double> points;
 };
 
-
-template<class T>
-class precise_ofstream : public std::ofstream
-{
-public:
-    precise_ofstream(std::string fn):std::ofstream(fn) {
-        auto max_digits = std::numeric_limits<T>::max_digits10;
-        this->precision(max_digits);
-        *this << std::scientific;
-    }
-};
-
-template<class T>
-class write_J_stream : public precise_ofstream<T>
-{
-public:
-    write_J_stream(std::string fn, size_t rows, size_t cols)
-        :precise_ofstream<T>(fn)
-    {
-        std::cout << "Writing to " << fn << std::endl;
-        if (!this->good()) {
-            std::cerr << "FAILED\n";
-            throw "oik";
-        }
-        *this << rows << " " << cols << std::endl;
-    }
-};
-
-
-void write_J(const std::string& fn, int Jrows, int Jcols, double** J);
-
-void write_J(const std::string& fn, int Jrows, int Jcols, double* J);
-
-void write_times(double tf, double tJ);
-
-void write_times(const std::string& fn, double tf, double tJ, double* t_sparsity = nullptr);
-
 #ifdef DO_EIGEN
 void read_hand_model(const std::string& path, HandModelEigen* pmodel);
 
@@ -90,13 +48,6 @@ void read_hand_model(const std::string& path, HandModelLightMatrix* pmodel);
 
 void read_hand_instance(const std::string& model_dir, const std::string& fn_in,
     std::vector<double>* theta, HandDataLightMatrix* data, std::vector<double>* us = nullptr);
-
-void read_lstm_instance(const std::string& fn,
-    int* l, int* c, int* b,
-    std::vector<double>& main_params,
-    std::vector<double>& extra_params,
-    std::vector<double>& state,
-    std::vector<double>& sequence);
 
 // Time a function
 double timer(int nruns, double limit, std::function<void()> func);
