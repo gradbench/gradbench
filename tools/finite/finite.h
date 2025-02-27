d37bacf1bc05f28bfc8b8e58d0dd447d7b9facb8
Troels Henriksen <athas@sigkill.dk>
2025-02-27 17:02:51 +0100

    Implement kmeans with finite differences.
    This also rewrites finite.h to support higher order derivatives.
    
    Marked as draft because it fails to validate on one of the (randomly
    generated) datasets. This may simply be a fundamental problem with
    applying finite differences to this problem.

--- tools/finite/finite.h
+++ tools/finite/finite.h
@@ -30,11 +30,18 @@ const double FINITE_DIFFERENCES_DEFAULT_ZERO_NEIGHBORHOOD_CHARACTERISTIC_SCALE =
 ///     gives the difference to use in finite differentiation when the absolute value
 ///     of the said variable is lesser than zero_neighborhood_radius</param>
 template<typename T>
-std::function<T(T)> finite_differences_default_step_function(T epsilon = FINITE_DIFFERENCES_DEFAULT_EPSILON, T zero_neighborhood_radius = FINITE_DIFFERENCES_DEFAULT_ZERO_NEIGHBORHOOD_RADIUS, T zero_neighborhood_characteristic_scale = FINITE_DIFFERENCES_DEFAULT_ZERO_NEIGHBORHOOD_CHARACTERISTIC_SCALE) {
-    return [epsilon, zero_neighborhood_radius, zero_neighborhood_characteristic_scale](T x) {
-        T absx = std::abs(x);
-        return absx >= zero_neighborhood_radius ? absx * epsilon : zero_neighborhood_characteristic_scale * epsilon;
-    };
+std::function<T(T)> finite_differences_default_step_function
+(T epsilon = FINITE_DIFFERENCES_DEFAULT_EPSILON,
+ T zero_neighborhood_radius = FINITE_DIFFERENCES_DEFAULT_ZERO_NEIGHBORHOOD_RADIUS,
+ T zero_neighborhood_characteristic_scale = FINITE_DIFFERENCES_DEFAULT_ZERO_NEIGHBORHOOD_CHARACTERISTIC_SCALE) {
+  return [epsilon,
+          zero_neighborhood_radius,
+          zero_neighborhood_characteristic_scale](T x) {
+    T absx = std::abs(x);
+    return absx >= zero_neighborhood_radius
+      ? absx * epsilon
+      : zero_neighborhood_characteristic_scale * epsilon;
+  };
 }
 
 // Engine for arrpoximate differentiation using finite differences.
@@ -42,102 +49,143 @@ std::function<T(T)> finite_differences_default_step_function(T epsilon = FINITE_
 // Encapsulates a function that chooses the step for finite differences
 // based on the input value.
 template<typename T>
-class FiniteDifferencesEngine
-{
+class FiniteDifferencesEngine {
 private:
-    std::vector<T> tmp_output_f;
-    std::vector<T> tmp_output_b;
-    int max_output_size;
+  std::vector<T> tmp_output_f;
+  std::vector<T> tmp_output_b;
+  int max_output_size;
 
-    // VECTOR UTILS
+  // VECTOR UTILS
 
-    // Subtract B from A
-    static T* sub_vec(T* a, const T* b, int sz) {
-        for (int i = 0; i < sz; ++i)
-            a[i] -= b[i];
-        return a;
+  // Add B to A
+  static std::vector<T>& add_vec(std::vector<T>& a, const std::vector<T>& b) {
+    int sz = a.size();
+    for (int i = 0; i < sz; ++i)
+      a[i] += b[i];
+    return a;
+  }
+
+  // Subtract B from A
+  static std::vector<T>& sub_vec(std::vector<T>& a, const std::vector<T>& b) {
+    int sz = a.size();
+    for (int i = 0; i < sz; ++i)
+      a[i] -= b[i];
+    return a;
+  }
+
+  // Scale vector A by scalar B
+  static std::vector<T>& scale_vec(std::vector<T>& a, T b) {
+    int sz = a.size();
+    for (int i = 0; i < sz; ++i)
+      a[i] *= b;
+    return a;
+  }
+
+  // Divide vector A by scalar B
+  static std::vector<T>& div_vec(std::vector<T>& a, T b) {
+    return scale_vec(a, 1/b);
+  }
+
+  // Insert B starting at a point in A
+  static T* vec_ins(T* a, const T* b, int b_sz) {
+    for (int i = 0; i < b_sz; ++i)
+      a[i] = b[i];
+    return a;
+  }
+
+  // Compute binomial coefficient.
+  static long long binom(int n, int k) {
+    if (k > n) return 0;
+    if (k == 0 || k == n) return 1;
+
+    k = std::min(k, n - k);
+    long long result = 1;
+
+    for (int i = 0; i < k; ++i) {
+      result *= (n - i);
+      result /= (i + 1);
     }
 
-    // Divide vector A by scalar B
-    static T* div_vec(T* a, int a_sz, T b) {
-        for (int i = 0; i < a_sz; ++i)
-            a[i] /= b;
-        return a;
-    }
-
-    // Insert B starting at a point in A
-    static T* vec_ins(T* a, const T* b, int b_sz) {
-        for (int i = 0; i < b_sz; ++i)
-            a[i] = b[i];
-        return a;
-    }
+    return result;
+  }
 
 public:
-    // max_output_size - maximum size of the ouputs of the functions
-    // this engine will be able to approximately differentiate.
-    // step_function - a function that chooses the step for finite differences
-    // based on the input value. Defaults to finite_differences_default_step_function().
-    // For fixed step use [](T x){ return STEP_VALUE; }
-    FiniteDifferencesEngine(int max_output_size = 0, std::function<T(T)> step_function = finite_differences_default_step_function<T>()): tmp_output_f(max_output_size), tmp_output_b(max_output_size), max_output_size(max_output_size), step(step_function)
-    {}
+  // max_output_size - maximum size of the ouputs of the functions
+  // this engine will be able to approximately differentiate.
+  // step_function - a function that chooses the step for finite
+  // differences based on the input value. Defaults to
+  // finite_differences_default_step_function(). For fixed step use
+  // [](T x){ return STEP_VALUE; }
+  FiniteDifferencesEngine(int max_output_size = 0,
+                          std::function<T(T)> step_function = finite_differences_default_step_function<T>())
+    : tmp_output_f(max_output_size),
+      tmp_output_b(max_output_size),
+      max_output_size(max_output_size),
+      step(step_function)
+  {}
 
-    // Computes finite differences step for given argument.
-    std::function<T(T)> step;
+  // Computes finite differences step for given argument.
+  std::function<T(T)> step;
 
-    // sets max_output_size - maximum size of the ouputs of the functions
-    // this engine is be able to approximately differentiate
-    void set_max_output_size(int size)
-    {
-        tmp_output_f.resize(size);
-        tmp_output_b.resize(size);
-        max_output_size = size;
+  // sets max_output_size - maximum size of the ouputs of the functions
+  // this engine is be able to approximately differentiate
+  void set_max_output_size(int size) {
+    tmp_output_f.resize(size);
+    tmp_output_b.resize(size);
+    max_output_size = size;
+  }
+
+  // gets max_output_size - maximum size of the ouputs of the functions
+  // this engine is be able to approximately differentiate
+  int current_max_output_size() {
+    return max_output_size;
+  }
+
+  // FINITE DIFFERENTIATION FUNCTION
+
+  /// <summary>Approximately differentiate a function using finite differences
+  ///     Step for differentiation is determined separately for every input
+  ///     by calling this.step(input)</summary>
+  ///
+  /// <param name="order">The order of the derivative to compute.</param>
+  ///
+  /// <param name="func">Function to be differentiated.
+  ///		Should accept 2 pointers as arguments (one input, one output).
+  ///		Each output will be differentiated with respect to each input.</param>
+  /// <param name="input">Pointer to input data (scalar or vector)</param>
+  /// <param name="input_size">Input data size (1 for scalar)</param>
+  /// <param name="output_size">Size of 'func' output data</param>
+  /// <param name="result">Pointer to where resultant Jacobian should go.
+  ///		Will be stored as a vector(input_size * output_size).
+  ///		Will store in format foreach (input) { foreach (output) {} }</param>
+  void finite_differences(int order,
+                          std::function<void(T*, T*)> func,
+                          T* input, int input_size, int output_size,
+                          T* result) {
+    for (int i = 0; i < input_size; i++) {
+      T input_i_orig = input[i];
+      T delta = step(input_i_orig);
+      T tmp_b = input_i_orig - delta;
+      T dx = delta * 2;
+      T tmp_f = tmp_b + dx;
+      // adjusting dx so that (tmp_b + dx) - tmp_b == dx
+      dx = tmp_f - tmp_b;
+      if (dx < delta * 0.5)
+        std::cerr << "WARNING: Finite difference step "
+                  << delta << " seems incompatible with the argument "
+                  << input_i_orig << std::endl;
+
+      std::fill(tmp_output_f.begin(), tmp_output_f.end(), 0);
+      for (int j = 0; j < order+1; j++) {
+        input[i] = input_i_orig + (order/2.0-j)*dx;
+        func(input, tmp_output_b.data());
+        scale_vec(tmp_output_b, binom(order,j) * (j%2 == 0 ? 1 : -1));
+        add_vec(tmp_output_f, tmp_output_b);
+      }
+      input[i] = input_i_orig;
+
+      div_vec(tmp_output_f, pow(dx,order));
+      vec_ins(&result[output_size * i], tmp_output_f.data(), output_size);
     }
-
-    // gets max_output_size - maximum size of the ouputs of the functions
-    // this engine is be able to approximately differentiate
-    int current_max_output_size()
-    {
-        return max_output_size;
-    }
-
-    // FINITE DIFFERENTIATION FUNCTION
-
-    /// <summary>Approximately differentiate a function using finite differences
-    ///     Step for differentiation is determined separately for every input
-    ///     by calling this.step(input)</summary>
-    /// <param name="func">Function to be differentiated.
-    ///		Should accept 2 pointers as arguments (one input, one output).
-    ///		Each output will be differentiated with respect to each input.</param>
-    /// <param name="input">Pointer to input data (scalar or vector)</param>
-    /// <param name="input_size">Input data size (1 for scalar)</param>
-    /// <param name="output_size">Size of 'func' output data</param>
-    /// <param name="result">Pointer to where resultant Jacobian should go.
-    ///		Will be stored as a vector(input_size * output_size).
-    ///		Will store in format foreach (input) { foreach (output) {} }</param>
-    void finite_differences(std::function<void(T*, T*)> func,
-        T* input, int input_size, int output_size,
-        T* result)
-    {
-        volatile T tmp_f, tmp_b;
-        for (int i = 0; i < input_size; i++)
-        {
-            T originalInput = input[i];
-            T delta = step(originalInput);
-            tmp_b = originalInput - delta;
-            T dx = delta * 2;
-            tmp_f = tmp_b + dx;
-            // adjusting dx so that (tmp_b + dx) - tmp_b == dx
-            dx = tmp_f - tmp_b;
-            if (dx < delta * 0.5)
-                std::cerr << "WARNING: Finite difference step " << delta << " seems incompatible with the argument " << originalInput << std::endl;
-
-            input[i] = tmp_f;
-            func(input, tmp_output_f.data());
-            input[i] = tmp_b;
-            func(input, tmp_output_b.data());
-            div_vec(sub_vec(tmp_output_f.data(), tmp_output_b.data(), output_size), output_size, dx);
-            vec_ins(&result[output_size * i], tmp_output_f.data(), output_size);
-            input[i] = originalInput;
-        }
-    }
+  }
 };
