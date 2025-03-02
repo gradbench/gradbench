ada8f1eedfe1806a5eff521323d5e139e722c457
Troels Henriksen <athas@sigkill.dk>
2025-03-02 19:32:50 +0100

    Modernise Zygote.

--- tools/zygote/run.jl
+++ tools/zygote/run.jl
@@ -1,18 +1,4 @@
-module hello
-
-import Zygote
-
-function square(x)
-  return x * x
-end
-
-function double(x)
-  z, = Zygote.gradient(square, x)
-  return z
-end
-
-end
-
+include("./hello.jl")
 import JSON
 
 function resolve(mod, name)
