f0f8be9c4fb0945e1fbf8a170fdc8c467c5a4cca
Troels Henriksen <athas@sigkill.dk>
2025-03-02 14:57:50 +0100

    Improve kmeans::objective
    The __restrict__ annotations significantly helps Enzyme. The use of
    std::min could theoretically help tools based on operator overloading,
    but I don't have any clear evidence that it does. It does make the
    code look slightly nicer.

--- cpp/gradbench/evals/kmeans.hpp
+++ cpp/gradbench/evals/kmeans.hpp
@@ -17,18 +17,15 @@ T euclid_dist_2(int d, T const *a, T const *b) {
 
 template<typename T>
 void objective(int n, int k, int d,
-               T const *points, T const *centroids,
-               T* err) {
+               T const * __restrict__ points, T const * __restrict__ centroids,
+               T* __restrict__ err) {
   T cost = 0;
   for (int i = 0; i < n; i++) {
     T const *a = &points[i*d];
     T closest = INFINITY;
     for (int j = 0; j < k; j++) {
       T const *b = &centroids[j*d];
-      T dist = euclid_dist_2(d, a, b);
-      if (dist < closest) {
-        closest = dist;
-      }
+      closest = std::min(closest, euclid_dist_2(d, a, b));
     }
     cost += closest;
   }
