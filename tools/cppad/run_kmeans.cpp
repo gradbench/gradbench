901bdce5935566c9c06659c3609bb69ede14fbed
Troels Henriksen <athas@sigkill.dk>
2025-03-02 11:53:39 +0100

    Paranoia check.

--- tools/cppad/run_kmeans.cpp
+++ tools/cppad/run_kmeans.cpp
@@ -52,6 +52,8 @@ public:
       }
     }
 
+    // Provide row and column information. We only wish to compute the
+    // diagonal.
     for (size_t i = 0; i < input_size; i++) {
       _row[i] = _col[i] = i;
     }
@@ -67,7 +69,9 @@ public:
     std::vector<double> w(1);
     w[0] = 1;
 
-    size_t nsweep = _tape->SparseHessian(_input.centroids, w, _p, _row, _col, _H, _work);
+    size_t nsweep =
+      _tape->SparseHessian(_input.centroids, w, _p, _row, _col, _H, _work);
+    assert(nsweep == 1); // Just a precaution.
 
     for (int i = 0; i < _input.k * _input.d; i++) {
       output.dir[i] = _J[i] / _H[i];
