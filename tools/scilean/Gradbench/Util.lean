496c8bd6ca90be1e6299f62650aecb75b3804215
lecopivo <skrivantomas@seznam.cz>
2025-03-08 16:27:44 -0500

    update evals.txt and respect min_runs and min_seconds

--- tools/scilean/Gradbench/Util.lean
+++ tools/scilean/Gradbench/Util.lean
@@ -36,17 +36,24 @@ structure Response where
   timings : List Timing
 deriving ToJson
 
-
-
 def wrap [FromJson a] [ToJson b] (f : a -> b) (x' : Json)
     : Except String (IO Output) := do
-  -- let min_runs ← x'.getObjVal? "min_runs" >>= Json.getNat?
-  -- let min_seconds ← x'.getObjVal? "min_seconds" >>= Json.getNat?
+  let min_runs ← x'.getObjVal? "min_runs" >>= Json.getNat?
+  let min_seconds ← x'.getObjVal? "min_seconds" >>= Json.getNat?
   let x : a <- fromJson? x'
   return do
-    let start <- IO.monoNanosNow
-    let y ← pure (f x)
-    let done <- IO.monoNanosNow
-    let y' := toJson y
-    let timings := [{ name := "evaluate", nanoseconds := done - start }]
+    let mut totalRuns := 0
+    let mut totalNs := 0
+    let mut timings : List Timing := []
+    let mut y' : Json := default
+    while (totalRuns < min_runs) || (totalNs < 10^9 * min_seconds) do
+      let start <- IO.monoNanosNow
+      let y ← pure (f x)
+      let done <- IO.monoNanosNow
+      if totalRuns = 0 then
+        y' := toJson y
+      let time := done - start
+      totalNs := totalNs + time
+      totalRuns := totalRuns + 1
+      timings := { name := "evaluate", nanoseconds := time } :: timings
     return { output := y', timings }
