258d08b1d8e38b07042d5c2439cad2fc691b2969
Troels Henriksen <athas@sigkill.dk>
2025-03-13 13:47:38 +0100

    ht: stick to one workload class
    I'm indecisive so I retained the ability to run the different classes
    with explicit arguments passed to the eval.
    
    Fixes #367.

--- python/gradbench/gradbench/evals/ht/run.py
+++ python/gradbench/gradbench/evals/ht/run.py
@@ -28,6 +28,8 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--min", type=int, default=1)
     parser.add_argument("--max", type=int, default=12)
+    parser.add_argument("--model", type=str, choices=['small', 'big'], default='big')
+    parser.add_argument("--variant", type=str, choices=['complicated', 'simple'], default='complicated')
     parser.add_argument("--min-runs", type=int, default=1)
     parser.add_argument("--min-seconds", type=float, default=1)
     parser.add_argument("--no-validation", action="store_true", default=False)
@@ -39,35 +41,25 @@ def main():
     e.start()
     if e.define().success:
         data_root = Path("evals/ht/data")  # assumes cwd is set correctly
-        # NOTE: data files are taken directly from ADBench.
-        simple_small = data_root / "simple_small"
-        simple_big = data_root / "simple_big"
-        complicated_small = data_root / "complicated_small"
-        complicated_big = data_root / "complicated_big"
+        data_dir = data_root / f"{args.variant}_{args.model}"
 
-        def evals(data_dir, complicated):
-            c = os.path.basename(os.path.normpath(data_dir)) + "/"
-            for i in range(args.min, args.max + 1):
-                fn = next(data_dir.glob(f"hand{i}_*.txt"), None)
-                model_dir = data_dir / "model"
-                input = io.read_hand_instance(model_dir, fn, complicated).to_dict()
-                e.evaluate(
-                    function="objective",
-                    input=input
-                    | {"min_runs": args.min_runs, "min_seconds": args.min_seconds},
-                    description=c + fn.stem,
-                )
-                e.evaluate(
-                    function="jacobian",
-                    input=input
-                    | {"min_runs": args.min_runs, "min_seconds": args.min_seconds},
-                    description=c + fn.stem,
-                )
-
-        evals(simple_small, False)
-        evals(simple_big, False)
-        evals(complicated_small, True)
-        evals(complicated_big, True)
+        c = os.path.basename(os.path.normpath(data_dir)) + "/"
+        for i in range(args.min, args.max + 1):
+            fn = next(data_dir.glob(f"hand{i}_*.txt"), None)
+            model_dir = data_dir / "model"
+            input = io.read_hand_instance(model_dir, fn, args.variant == 'complicated').to_dict()
+            e.evaluate(
+                function="objective",
+                input=input
+                | {"min_runs": args.min_runs, "min_seconds": args.min_seconds},
+                description=fn.stem,
+            )
+            e.evaluate(
+                function="jacobian",
+                input=input
+                | {"min_runs": args.min_runs, "min_seconds": args.min_seconds},
+                description=fn.stem,
+            )
 
 
 if __name__ == "__main__":
