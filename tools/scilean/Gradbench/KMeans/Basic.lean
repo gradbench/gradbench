708fd19c004464b28d400edc8b70b324f0ec46c9
lecopivo <skrivantomas@seznam.cz>
2025-03-14 17:41:05 -0400

    add simp lemma that is missing from SciLean

--- tools/scilean/Gradbench/KMeans/Basic.lean
+++ tools/scilean/Gradbench/KMeans/Basic.lean
@@ -8,9 +8,15 @@ open SciLean Scalar
 
 set_default_scalar Float
 
+-- mising simp theorem from SciLean
+@[simp,simp_core]
+theorem vector_const_simp {n m} (i : Idx m) (j : Idx n) :
+    (VectorType.const 1 : Float^[n]^[m])[i,j] = 1 := by
+  simp only [getElem_curry,VectorType.Dense.const_spec]
+
+
 def kmeansObjective {n k d : ℕ} (points : Float^[d]^[n]) (centroids : Float^[d]^[k]) :=
   ∑ᴵ (i : Idx n), minᴵ (j : Idx k), ∑ᴵ (l : Idx d), (points[i,l] - centroids[j,l])^2
-
 
 def direction {n k d : ℕ} [NeZero k] (points : Float^[d]^[n]) (centroids : Float^[d]^[k]) : Float^[d]^[k] :=
   (let' ((_a,J),(_b,Hdiag)) :=
