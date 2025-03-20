77d041277d5411f7e80e2a38abb158672da150a5
Troels Henriksen <athas@sigkill.dk>
2025-03-20 14:00:30 +0100

    Explicit tool colors when plotting (#392)
    Co-authored-by: Sam Estep <sam@samestep.com>

--- packages/gradbench/src/Stats.tsx
+++ packages/gradbench/src/Stats.tsx
@@ -3,6 +3,29 @@ import { PlainObject, VegaLite } from "react-vega";
 import { TopLevelSpec } from "vega-lite";
 import "./Stats.css";
 
+// These colors have been determined by sampling the tool websites.
+// They are not picked to make a particularly pleasing scheme, and
+// some are unfortunately a little close to each other.
+const colors = {
+  adept: "#d8702f",
+  "adol-c": "#9da117",
+  codipack: "#d02718",
+  cppad: "#eeb10f",
+  enzyme: "#173559",
+  finite: "#aaaaaa",
+  floretta: "#f10537",
+  futhark: "#5f021f",
+  haskell: "#5e5086",
+  jax: "#5e98f6",
+  manual: "#000000",
+  ocaml: "#c24f1e",
+  pytorch: "#ee4c2c",
+  scilean: "#5c123a",
+  tapenade: "#047f01",
+  tensorflow: "#ff8d00",
+  zygote: "#6daa5e",
+};
+
 const makeSpec = ({
   title,
   yaxis,
@@ -29,6 +52,10 @@ const makeSpec = ({
       color: {
         field: "tool",
         type: "nominal",
+        scale: {
+          domain: Object.keys(colors),
+          range: Object.values(colors),
+        },
         legend: { values: tools },
       },
       x: {
@@ -110,6 +137,7 @@ export const Stats = ({ url }: { url: string }) => {
       </p>
       <div className="chart-box">
         <VegaLite
+          renderer="svg"
           spec={makeSpec({
             title: "ratio",
             yaxis: "derivative / primal",
@@ -122,6 +150,7 @@ export const Stats = ({ url }: { url: string }) => {
       </div>
       <div className="chart-box">
         <VegaLite
+          renderer="svg"
           spec={makeSpec({
             title: "derivative",
             yaxis: "derivative (seconds)",
@@ -132,6 +161,7 @@ export const Stats = ({ url }: { url: string }) => {
       </div>
       <div className="chart-box">
         <VegaLite
+          renderer="svg"
           spec={makeSpec({
             title: "primal",
             yaxis: "primal (seconds)",
