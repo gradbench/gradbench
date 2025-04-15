4f2bc6561c9a7a0aee64bdb5bc020f6485ace624
Troels Henriksen <athas@sigkill.dk>
2025-04-15 18:54:52 +0200

    New tool: ad.hpp (#482)
    Co-authored-by: Sasha Fleming <57460414+aj-fleming@users.noreply.github.com>

---
+++ tools/ad-hpp/hello.cpp
@@ -0,0 +1,31 @@
+#include "gradbench/main.hpp"
+#include "gradbench/evals/hello.hpp"
+#include "ad.hpp"
+
+using adjoint_t = ad::adjoint_t<double>;
+using adjoint   = ad::adjoint<double>;
+
+class Double : public Function<hello::Input, hello::DoubleOutput> {
+public:
+  Double(hello::Input& input) : Function(input) {}
+
+  void compute(hello::DoubleOutput& output) {
+    adjoint::global_tape = adjoint::tape_t::create();
+
+    adjoint_t x = _input;
+    adjoint::global_tape->register_variable(x);
+
+    adjoint_t y = hello::square(x);
+
+    ad::derivative(y) = 1.0;
+    adjoint::global_tape->interpret_adjoint();
+    output = ad::derivative(x);
+  }
+};
+
+int main(int argc, char* argv[]) {
+  return generic_main(argc, argv, {
+      {"square", function_main<hello::Square>},
+      {"double", function_main<Double>}
+    });;
+}
---
+++ tools/ad-hpp/llsq.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include "gradbench/main.hpp"
+#include "gradbench/evals/llsq.hpp"
+#include "ad.hpp"
+
+using adjoint_t = ad::adjoint_t<double>;
+using adjoint   = ad::adjoint<double>;
+
+class Gradient : public Function<llsq::Input, llsq::GradientOutput> {
+  std::vector<adjoint_t> _x;
+public:
+  Gradient(llsq::Input& input) :
+    Function(input),
+    _x(_input.x.size()) {
+    size_t m = _input.x.size();
+    adjoint::global_tape = adjoint::tape_t::create();
+    for (size_t i = 0; i < m; i++) {
+      _x[i] = _input.x[i];
+    }
+  }
+
+  void compute(llsq::GradientOutput& output) {
+    size_t n = _input.n;
+    size_t m = _input.x.size();
+
+    output.resize(m);
+
+    adjoint::global_tape->reset();
+
+    for (size_t i = 0; i < m; i++) {
+      adjoint::global_tape->register_variable(_x[i]);
+    }
+
+    adjoint_t y;
+
+    llsq::primal<adjoint_t>(n, m, _x.data(), &y);
+
+    ad::derivative(y) = 1.0;
+    adjoint::global_tape->interpret_adjoint();
+
+    for (size_t i = 0; i < m; i++) {
+      output[i] = ad::derivative(_x[i]);
+    }
+  }
+};
+
+int main(int argc, char* argv[]) {
+  return generic_main(argc, argv, {
+      {"primal", function_main<llsq::Primal>},
+      {"gradient", function_main<Gradient>},
+    });;
+}
