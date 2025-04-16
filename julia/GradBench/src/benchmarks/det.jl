4fe369a0dcf77633ea52b68a3a0293d4d54ff0b5
Troels Henriksen <athas@sigkill.dk>
2025-04-16 11:17:27 +0200

    Provide two implementations of det.
    One is pure and therefore works with Zygote, while the other is much
    faster but uses mutation.

--- julia/GradBench/src/benchmarks/det.jl
+++ julia/GradBench/src/benchmarks/det.jl
@@ -1,3 +1,5 @@
+# This module provides two implementations: one that requires
+# mutation, and one that does not.
 module Det
 
 export Input, input_from_json, det_by_minor, primal
@@ -10,6 +12,10 @@ end
 function input_from_json(j)
     Input(j["A"], j["ell"])
 end
+
+module Pure
+
+using ..Det
 
 function minor(matrix::AbstractMatrix{T}, row::Int, col::Int) where T
     matrix[[1:row-1; row+1:end], [1:col-1; col+1:end]]
@@ -34,6 +40,70 @@ end
 
 function primal(input::Input)
     return det_by_minor(transpose(reshape(input.A, input.ell, input.ell)))
+end
+
+end
+
+# This is written in a very imperative style mirroring the one used
+# for C++.
+module Impure
+
+using ..Det
+
+function det_of_minor(A::AbstractMatrix{T},
+                      n::Int,
+                      m::Int,
+                      r::Vector{Int},
+                      c::Vector{Int}) where T
+    R0 = r[n + 1]
+    @assert R0 <= n
+    Cj = c[n + 1]
+    @assert Cj <= n
+
+    if m == 1
+        return A[R0, Cj]
+    end
+
+    detM = zero(T)
+    sign = 1
+    r[n + 1] = r[R0 + 1]
+    Cj1 = n
+
+    for j in 1:m
+        M0j = A[R0, Cj]
+
+        c[Cj1 + 1] = c[Cj + 1]
+        detS = det_of_minor(A, n, m - 1, r, c)
+        c[Cj1 + 1] = Cj
+
+        if sign > 0
+            detM += M0j * detS
+        else
+            detM -= M0j * detS
+        end
+
+        Cj1 = Cj
+        Cj = c[Cj + 1]
+        sign = -sign
+    end
+
+    r[n + 1] = R0
+    return detM
+end
+
+function det_by_minor(A)
+    ell = size(A, 1)
+    r = Vector(1:ell+1)
+    r[end] = 1
+    c = Vector(1:ell+1)
+    c[end] = 1
+    return det_of_minor(A, ell, ell, r, c)
+end
+
+function primal(input::Input)
+    return det_by_minor(transpose(reshape(input.A, input.ell, input.ell)))
+end
+
 end
 
 end
--- tools/enzyme-jl/run_det.jl
+++ tools/enzyme-jl/run_det.jl
@@ -6,12 +6,12 @@ import GradBench
 function gradient(j)
     input = GradBench.Det.input_from_json(j)
     M = transpose(reshape(input.A, input.ell, input.ell))
-    z, = Enzyme.gradient(Reverse, GradBench.Det.det_by_minor, M)
+    z, = Enzyme.gradient(Reverse, GradBench.Det.Impure.det_by_minor, M)
     return reshape(z', input.ell*input.ell)
 end
 
 GradBench.register!("det", Dict(
-    "primal" => GradBench.Det.primal ∘ GradBench.Det.input_from_json,
+    "primal" => GradBench.Det.Impure.primal ∘ GradBench.Det.input_from_json,
     "gradient" => gradient
 ))
 
