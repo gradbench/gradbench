e21d0d14c3f88c3c02541943fef9195d149423b6
Troels Henriksen <athas@sigkill.dk>
2025-04-16 17:53:34 +0200

    Move out get_Qs.

--- tools/enzyme-jl/run_gmm.jl
+++ tools/enzyme-jl/run_gmm.jl
@@ -3,35 +3,27 @@ module GMM
 using Enzyme
 import GradBench
 
-function get_Qs(x, means, icfs)
-    d = size(x, 1)
-    k = size(means, 2)
-    Qs = cat([GradBench.GMM.get_Q(d, icfs[:, ik]) for ik in 1:k]...;
-             dims=[3])
-    Qs
-end
-
 function objective(input)
     gmm_input = GradBench.GMM.input_from_json(input)
-    Qs = get_Qs(gmm_input.x, gmm_input.means, gmm_input.icfs)
+    k = size(gmm_input.means, 2)
+    d = size(gmm_input.x, 1)
+    Qs = GradBench.GMM.get_Qs(gmm_input.icfs, k, d)
     return GradBench.GMM.objective(gmm_input.alphas, gmm_input.means, Qs, gmm_input.x, gmm_input.wishart)
 end
 
 function jacobian(input)
     gmm_input = GradBench.GMM.input_from_json(input)
+    k = size(gmm_input.means, 2)
+    d = size(gmm_input.x, 1)
+    Qs = GradBench.GMM.get_Qs(gmm_input.icfs, k, d)
 
-    function wrap(alpha, means, icfs, x, wishart)
-        Qs = get_Qs(x, means, icfs)
-        GradBench.GMM.objective(alpha, means, Qs, x, wishart)
-    end
-
-    (d_alphas, d_means, d_icfs) =
+    J =
         Enzyme.gradient(Reverse,
-                        wrap,
-                        gmm_input.alphas, gmm_input.means, gmm_input.icfs,
+                        GradBench.GMM.objective,
+                        gmm_input.alphas, gmm_input.means, Qs,
                         Const(gmm_input.x), Const(gmm_input.wishart))
 
-    vcat(vec(d_alphas), vec(d_means), vec(d_icfs))
+    GradBench.GMM.pack_J(J, k, d)
 end
 
 GradBench.register!("gmm", Dict(
