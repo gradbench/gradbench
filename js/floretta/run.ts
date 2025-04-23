02394072721a9d389eeb32d9f8168c3acf017a8e
Sam Estep <sam@samestep.com>
2025-04-23 12:53:18 -0400

    Port Floretta code from JavaScript to TypeScript (#543)

--- js/floretta/run.ts
+++ js/floretta/run.ts
@@ -1,29 +1,43 @@
 import { stdin } from "node:process";
 import * as readline from "node:readline";
+import type {
+  DefineMessage,
+  DefineResponse,
+  EvaluateMessage,
+  EvaluateResponse,
+  Message,
+} from "./protocol.ts";
 
-const catchErrors = async (f, ...args) => {
+const catchErrors = async <T extends any[]>(
+  f: (...args: T) => Promise<{ success: boolean }>,
+  ...args: T
+): Promise<{ success: boolean; error?: string }> => {
   try {
     return await f(...args);
-  } catch (error) {
+  } catch (error: any) {
     return { success: false, error: `${error.stack}` };
   }
 };
 
-const getModule = async (name) => await import(`./${name}.js`);
+const getModule = async (name: string) => await import(`./${name}.ts`);
 
-const define = async (message) => {
+const define = async (
+  message: DefineMessage,
+): Promise<Omit<DefineResponse, "id">> => {
   await getModule(message.module);
   return { success: true };
 };
 
-const evaluate = async (message) => {
+const evaluate = async (
+  message: EvaluateMessage,
+): Promise<Omit<EvaluateResponse, "id">> => {
   const module = await getModule(message.module);
   const response = await module[message.function](message.input);
   return { success: true, ...response };
 };
 
 for await (const line of readline.createInterface({ input: stdin })) {
-  const message = JSON.parse(line);
+  const message: Message = JSON.parse(line);
   const response = { id: message.id };
   switch (message.kind) {
     case "define": {
