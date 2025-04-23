43cbf998b16a4dca5fc23d267e9fa810a84ecbeb
Sasha Fleming <alexander.fleming@rwth-aachen.de>
2025-04-23 13:10:24 +0200

    move allocation of adjoint vector to constructor;  - all good ideas get stolen

--- tools/ad-hpp/kmeans.cpp
+++ tools/ad-hpp/kmeans.cpp
@@ -11,23 +11,16 @@ class Dir : public Function<kmeans::Input, kmeans::DirOutput> {
   using tape_t         = adjoint::tape_t;
   using tape_options_t = adjoint::tape_options_t;
 
+  std::vector<active_t> centroids_active;
+  tape_t::position_t    tape_start_pos;
+
 public:
-  Dir(kmeans::Input& input) : Function(input) {
-    // 800 MiB?!?!
+  Dir(kmeans::Input& input)
+      : Function(input), centroids_active(input.centroids.size()) {
+    // 800 MiB blob tape seems ok.
     long int       tape_size = 1024 * 1024 * 800;
     tape_options_t opts(tape_size);
     adjoint::global_tape = tape_t::create(opts);
-  }
-  ~Dir() { tape_t::remove(adjoint::global_tape); }
-
-  void compute(kmeans::DirOutput& output) {
-    output.k = _input.k;
-    output.d = _input.d;
-    output.dir.resize(_input.k * _input.d);
-
-    tape_t* gtape = adjoint::global_tape;
-    gtape->reset();
-    std::vector<active_t> centroids_active(_input.centroids.size());
     for (size_t i = 0; i < centroids_active.size(); i++) {
       // set value
       ad::passive_value(centroids_active[i]) = _input.centroids[i];
@@ -37,15 +30,24 @@ public:
       ad::derivative(ad::value(centroids_active[i])) = 1.0;
       // clear {c_i}_(1)^(2)
       ad::derivative(ad::derivative(centroids_active[i])) = 0;
-      gtape->register_variable(centroids_active[i]);
+      adjoint::global_tape->register_variable(centroids_active[i]);
     }
+    tape_start_pos = adjoint::global_tape->get_position();
+  }
+  ~Dir() { tape_t::remove(adjoint::global_tape); }
 
+  void compute(kmeans::DirOutput& output) {
+    output.k = _input.k;
+    output.d = _input.d;
+    output.dir.resize(_input.k * _input.d);
+    adjoint::global_tape->reset_to(tape_start_pos);
+    // input is already set!
     active_t active_err;
     kmeans::objective(_input.n, _input.k, _input.d, _input.points.data(),
                       centroids_active.data(), &active_err);
 
     ad::value(ad::derivative(active_err)) = 1.0;
-    gtape->interpret_adjoint();
+    adjoint::global_tape->interpret_adjoint();
     for (size_t i = 0; i < centroids_active.size(); i++) {
       // compute J_i * H^-1_{ii} and write to out.dir[i]
       output.dir[i] = ad::value(ad::derivative(centroids_active[i])) /
