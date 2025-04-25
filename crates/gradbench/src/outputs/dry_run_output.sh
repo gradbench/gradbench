f13370624515bcc51c0fa260c6692e6b736f426a
Sam Estep <sam@samestep.com>
2025-04-25 10:46:42 -0400

    Fix tests

--- crates/gradbench/src/outputs/dry_run_output.sh
+++ crates/gradbench/src/outputs/dry_run_output.sh
@@ -1,8 +1,8 @@
-docker build . --file evals/norf/Dockerfile --tag ghcr.io/gradbench/eval-norf
-docker build . --file evals/qux/Dockerfile --tag ghcr.io/gradbench/eval-qux
-docker build . --file tools/bar/Dockerfile --tag ghcr.io/gradbench/tool-bar
-docker build . --file tools/baz/Dockerfile --tag ghcr.io/gradbench/tool-baz
-docker build . --file tools/foo/Dockerfile --tag ghcr.io/gradbench/tool-foo
+docker build . --file evals/norf/Dockerfile --tag ghcr.io/gradbench/eval-norf:latest
+docker build . --file evals/qux/Dockerfile --tag ghcr.io/gradbench/eval-qux:latest
+docker build . --file tools/bar/Dockerfile --tag ghcr.io/gradbench/tool-bar:latest
+docker build . --file tools/baz/Dockerfile --tag ghcr.io/gradbench/tool-baz:latest
+docker build . --file tools/foo/Dockerfile --tag ghcr.io/gradbench/tool-foo:latest
 mkdir -p 'a directory/norf' 'a directory/qux'
 gradbench run --eval 'docker run --rm --interactive ghcr.io/gradbench/eval-norf:latest' --tool 'docker run --rm --interactive ghcr.io/gradbench/tool-bar:latest' -o 'a directory/norf/bar.jsonl'
 gradbench run --eval 'docker run --rm --interactive ghcr.io/gradbench/eval-norf:latest' --tool 'docker run --rm --interactive ghcr.io/gradbench/tool-baz:latest' -o 'a directory/norf/baz.jsonl'
