7dec4c9a0c3f64ee6bd3468f83a3192f8fe8922a
Sam Estep <sam@samestep.com>
2025-05-06 15:58:41 -0400

    Implement `llsq`

---
+++ js/tensorflow/llsq.ts
@@ -0,0 +1,38 @@
+import type { Runs } from "@gradbench/common/protocol.ts";
+import { multipleRuns } from "@gradbench/common/util.ts";
+import * as tf from "@tensorflow/tfjs";
+
+const f =
+  (n: number) =>
+  (x: tf.Tensor): tf.Tensor => {
+    let t = tf
+      .range(0, n)
+      .mul(2 / (n - 1))
+      .sub(1);
+    let s = t.sign();
+    let c = tf.onesLike(t);
+    for (let j = 0; j < x.size; ++j) {
+      const xj = x.slice(j, 1);
+      s = s.sub(c.mul(xj));
+      c = c.mul(t);
+    }
+    return s.square().sum().div(2);
+  };
+
+interface Input extends Runs {
+  x: number[];
+  n: number;
+}
+
+export const primal = multipleRuns(({ x, n }: Input) => {
+  return () => {
+    return f(n)(tf.tensor(x)).arraySync();
+  };
+});
+
+export const gradient = multipleRuns(({ x, n }: Input) => {
+  const g = tf.grad(f(n));
+  return () => {
+    return g(tf.tensor(x)).arraySync();
+  };
+});
