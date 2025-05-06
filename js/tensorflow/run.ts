3427b1ea06ff9ceca120ad4c865aaabaeab234c8
Sam Estep <sam@samestep.com>
2025-05-06 18:53:49 -0400

    Explicitly check for `--precision=single`

--- js/tensorflow/run.ts
+++ js/tensorflow/run.ts
@@ -1,3 +1,11 @@
 import { main } from "@gradbench/common/util.ts";
+import { parseArgs } from "node:util";
 
-main({ getModule: async (name: string) => await import(`./${name}.ts`) });
+const { values } = parseArgs({ options: { precision: { type: "string" } } });
+let getModule = async (name: string) => await import(`./${name}.ts`);
+if (values.precision !== "single") {
+  getModule = () => {
+    throw Error("TensorFlow.js only supports single-precision floating point.");
+  };
+}
+main({ getModule });
