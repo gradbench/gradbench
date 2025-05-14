7b9ac94f1db5671e6fc850d5bde35b60ad4bdfc5
Troels Henriksen <athas@sigkill.dk>
2025-05-14 16:11:36 +0200

    NixOS workaround

--- scripts/lint/python.sh
+++ scripts/lint/python.sh
@@ -1,2 +1,13 @@
-uv run ruff check
-uv run ruff format --check
+#!/bin/sh
+
+# (Some?) NixOS systems cannot run the ruff installed by uv, unless we fake an
+# FHS environment with steam-run.
+
+if which steam-run >/dev/null; then
+    ruff="steam-run ruff"
+else
+    ruff=ruff
+fi
+
+uv run $ruff check
+uv run $ruff format --check
