89b9710888f7a3ce54e562cf6d2e184a66ad8076
Troels Henriksen <athas@sigkill.dk>
2025-05-15 17:54:07 +0200

    finite

--- tools/finite/gmm.cpp
+++ tools/finite/gmm.cpp
@@ -18,36 +18,51 @@ public:
   }
 
   void compute(gmm::JacOutput& output) {
-    int Jcols = (_input.k * (_input.d + 1) * (_input.d + 2)) / 2;
-    output.resize(Jcols);
+    const int l_sz = _input.d * (_input.d - 1) / 2;
+
+    output.d = _input.d;
+    output.k = _input.k;
+    output.n = _input.n;
+
+    output.alpha.resize(output.k);
+    output.mu.resize(output.k * output.d);
+    output.q.resize(output.k * output.d);
+    output.l.resize(output.k * l_sz);
 
     _engine.finite_differences(
         1,
         [&](double* alphas_in, double* err) {
           gmm::objective(_input.d, _input.k, _input.n, alphas_in,
-                         _input.means.data(), _input.icf.data(),
+                         _input.mu.data(), _input.q.data(), _input.l.data(),
                          _input.x.data(), _input.wishart, err);
         },
-        _input.alphas.data(), _input.alphas.size(), 1, output.data());
+        _input.alphas.data(), _input.alphas.size(), 1, output.alpha.data());
 
     _engine.finite_differences(
         1,
-        [&](double* means_in, double* err) {
+        [&](double* mu_in, double* err) {
           gmm::objective(_input.d, _input.k, _input.n, _input.alphas.data(),
-                         means_in, _input.icf.data(), _input.x.data(),
-                         _input.wishart, err);
+                         mu_in, _input.q.data(), _input.l.data(),
+                         _input.x.data(), _input.wishart, err);
         },
-        _input.means.data(), _input.means.size(), 1, &output.data()[_input.k]);
+        _input.mu.data(), _input.mu.size(), 1, output.mu.data());
 
     _engine.finite_differences(
         1,
-        [&](double* icf_in, double* err) {
+        [&](double* q_in, double* err) {
           gmm::objective(_input.d, _input.k, _input.n, _input.alphas.data(),
-                         _input.means.data(), icf_in, _input.x.data(),
-                         _input.wishart, err);
+                         _input.mu.data(), q_in, _input.l.data(),
+                         _input.x.data(), _input.wishart, err);
         },
-        _input.icf.data(), _input.icf.size(), 1,
-        &output.data()[_input.k + _input.d * _input.k]);
+        _input.q.data(), _input.q.size(), 1, output.q.data());
+    _engine.finite_differences(
+        1,
+        [&](double* l_in, double* err) {
+          gmm::objective(_input.d, _input.k, _input.n, _input.alphas.data(),
+                         _input.mu.data(), _input.q.data(), l_in,
+                         _input.x.data(), _input.wishart, err);
+        },
+        _input.l.data(), _input.l.size(), 1, output.l.data());
   }
 };
 
