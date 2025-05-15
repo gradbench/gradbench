bf38278790aa8b48cfa47e8b8c2eab1f6ba0728e
Troels Henriksen <athas@sigkill.dk>
2025-05-15 23:12:52 +0200

    tapenade

--- tools/tapenade/gmm.cpp
+++ tools/tapenade/gmm.cpp
@@ -11,17 +11,44 @@
 #include "evals/gmm/gmm_b.h"
 
 class Jacobian : public Function<gmm::Input, gmm::JacOutput> {
+  std::vector<double> _J;
+
 public:
   Jacobian(gmm::Input& input) : Function(input) {}
 
   void compute(gmm::JacOutput& output) {
-    int Jcols = (_input.k * (_input.d + 1) * (_input.d + 2)) / 2;
-    output.resize(Jcols);
+    const int l_sz = _input.d * (_input.d - 1) / 2;
 
-    double* alphas_gradient_part = output.data();
-    double* means_gradient_part  = output.data() + _input.alphas.size();
-    double* icf_gradient_part =
-        output.data() + _input.alphas.size() + _input.means.size();
+    output.d = _input.d;
+    output.k = _input.k;
+    output.n = _input.n;
+
+    output.alpha.resize(output.k);
+    output.mu.resize(output.k * output.d);
+    output.q.resize(output.k * output.d);
+    output.l.resize(output.k * l_sz);
+
+    // Because the Tapenade program is difficult to modify, we use the
+    // ADBench-like approach of merging the Q and L arrays into a single icf
+    // array.
+    int k = _input.k, d = _input.d;
+    int icf_sz = d * (d + 1) / 2;
+
+    std::vector<double> icf(k * icf_sz);
+    std::vector<double> icf_d(k * icf_sz);
+
+    double* alpha_gradient_part = output.alpha.data();
+    double* mu_gradient_part  = output.mu.data();
+    double* icf_gradient_part = icf_d.data();
+
+    for (int i = 0; i < k; i++) {
+      for (int j = 0; j < d; j++) {
+        icf[i * icf_sz + j] = _input.q[i*d+j];
+      }
+      for (int j = d; j < icf_sz; j++) {
+        icf[i * icf_sz + j] = _input.l[i*l_sz+j-d];
+      }
+    }
 
     double tmp = 0.0;  // stores fictive output
     // (Tapenade doesn't calculate an original function in reverse mode)
@@ -29,10 +56,19 @@ public:
     double errb = 1.0;  // stores dY
     // (equals to 1.0 for gradient calculation)
 
-    gmm_objective_b(_input.d, _input.k, _input.n, _input.alphas.data(),
-                    alphas_gradient_part, _input.means.data(),
-                    means_gradient_part, _input.icf.data(), icf_gradient_part,
+    gmm_objective_b(d, k, _input.n, _input.alpha.data(),
+                    alpha_gradient_part, _input.mu.data(),
+                    mu_gradient_part, icf.data(), icf_gradient_part,
                     _input.x.data(), _input.wishart, &tmp, &errb);
+
+    for (int i = 0; i < k; i++) {
+      for (int j = 0; j < d; j++) {
+        output.q[i*d+j] = icf_d[i * icf_sz + j];
+      }
+      for (int j = d; j < icf_sz; j++) {
+        output.l[i*l_sz+j-d] = icf_d[i * icf_sz + j];
+      }
+    }
   }
 };
 
