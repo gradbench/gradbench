636775086103853133306cd0f81cf68b04816337
Sam Estep <sam@samestep.com>
2025-05-20 20:04:47 -0400

    Shorten PyTorch boilerplate

--- python/gradbench/gradbench/tools/pytorch/gmm.py
+++ python/gradbench/gradbench/tools/pytorch/gmm.py
@@ -1,26 +1,6 @@
-from dataclasses import dataclass
-
 import torch
 from gradbench import wrap
 from gradbench.tools.pytorch.gmm_objective import gmm_objective
-
-
-@dataclass
-class Independent:
-    alpha: torch.Tensor
-    mu: torch.Tensor
-    q: torch.Tensor
-    l: torch.Tensor
-
-
-@dataclass
-class Input(Independent):
-    d: int
-    k: int
-    n: int
-    x: torch.Tensor
-    m: torch.Tensor
-    gamma: float
 
 
 def prepare(input):
@@ -31,25 +11,25 @@ def prepare(input):
         return tensor(data, requires_grad=True)
 
     def torchify(*, d, k, n, x, m, gamma, alpha, mu, q, l, **_):
-        return Input(
-            d=d,
-            k=k,
-            n=n,
-            x=tensor(x),
-            m=tensor(m),
-            gamma=gamma,
-            alpha=active_tensor(alpha),
-            mu=active_tensor(mu),
-            q=active_tensor(q),
-            l=active_tensor(l),
-        )
+        return {
+            "d": d,
+            "k": k,
+            "n": n,
+            "x": tensor(x),
+            "m": tensor(m),
+            "gamma": gamma,
+            "alpha": active_tensor(alpha),
+            "mu": active_tensor(mu),
+            "q": active_tensor(q),
+            "l": active_tensor(l),
+        }
 
     return torchify(**input)
 
 
 @wrap.multiple_runs(pre=prepare, post=float)
 def objective(input):
-    return gmm_objective(**vars(input))
+    return gmm_objective(**input)
 
 
 def postprocess(input):
@@ -61,7 +41,7 @@ def postprocess(input):
             "l": l.grad.tolist(),
         }
 
-    return listify(**vars(input))
+    return listify(**input)
 
 
 @wrap.multiple_runs(pre=prepare, post=postprocess)
@@ -70,9 +50,9 @@ def jacobian(input):
         if grad is not None:
             grad.zero_()
 
-    zero(input.alpha.grad)
-    zero(input.mu.grad)
-    zero(input.q.grad)
-    zero(input.l.grad)
-    gmm_objective(**vars(input)).backward()
+    zero(input["alpha"].grad)
+    zero(input["mu"].grad)
+    zero(input["q"].grad)
+    zero(input["l"].grad)
+    gmm_objective(**input).backward()
     return input
