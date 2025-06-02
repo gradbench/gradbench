c6e8dd42c6861f780f3bbaa39091face8636e3bc
raph_5 <45399899+raph5@users.noreply.github.com>
2025-06-02 15:51:52 +0200

    Add additional information to summary (#564)
    Co-authored-by: Sam Estep <sam@samestep.com>

---
+++ js/website/src/components/App.tsx
@@ -0,0 +1,140 @@
+import { useEffect, useState } from "react";
+import {
+  EvalStats,
+  NotFoundError,
+  Summary,
+  downloadEvalStat,
+  downloadSummary,
+} from "../store.ts";
+import EvalViz from "./EvalViz.tsx";
+import Header from "./Header.tsx";
+import SummaryViz from "./SummaryViz.tsx";
+
+enum Status {
+  Loading = 0,
+  Error = 1,
+  NotFound = 2,
+  Ok = 3,
+}
+
+const windowUrl = new URL(window.location.href).searchParams;
+const commitQuery = windowUrl.get("commit");
+const dateQuery = windowUrl.get("date");
+
+const App = () => {
+  const commit = commitQuery;
+  const [date, setDate] = useState<string | null>(dateQuery);
+  const [activeEval, setActiveEval] = useState<string | null>(null);
+
+  const setDateAndUpdateUrl = (date: string | null) => {
+    const url = new URL(window.location.href);
+    if (date === null) {
+      url.searchParams.delete("date");
+    } else {
+      url.searchParams.set("date", date);
+    }
+    window.history.pushState(null, "", url.href);
+    setDate(date);
+  };
+
+  const [summary, setSummary] = useState<Summary | null>(null);
+  const [summaryStatus, setSummaryStatus] = useState(Status.Loading);
+  useEffect(() => {
+    setSummaryStatus(Status.Loading);
+    downloadSummary(date, commit)
+      .then((summary) => {
+        // A race condition can happen if successive downloadSummary call
+        // returns out of order. Then select summary will be the last recived.
+        // Though this is rare enough to pretend this does not happens
+        setSummary(summary);
+        // Here we call setDate while date is dependency of the useEffect hook.
+        // This will trigger a useless re-run of the useEffect. Though the
+        // preformance overhead is minimal.
+        if (summary.date) setDate(summary.date);
+        setSummaryStatus(Status.Ok);
+      })
+      .catch((err) => {
+        if (err instanceof NotFoundError) {
+          setSummaryStatus(Status.NotFound);
+        } else {
+          console.error(err);
+          setSummaryStatus(Status.Error);
+        }
+      });
+  }, [date, commit]);
+
+  const [evalStats, setEvalStats] = useState<EvalStats | null>(null);
+  const [evalStatsStatus, setEvalStatsStatus] = useState(Status.Loading);
+  useEffect(() => {
+    if (activeEval === null) return;
+    if (activeEval === "hello") {
+      setEvalStats(null);
+      setEvalStatsStatus(Status.Ok);
+      return;
+    }
+    setEvalStatsStatus(Status.Loading);
+    downloadEvalStat(date, commit, activeEval)
+      .then((evalStats) => {
+        // A race condition can happen if successive downloadEvalStat call
+        // returns out of order. Then select evalStats will be the last recived.
+        // Though this is rare enough to pretend this does not happens
+        setEvalStats(evalStats);
+        setEvalStatsStatus(Status.Ok);
+      })
+      .catch((err) => {
+        console.error(err);
+        setEvalStatsStatus(Status.Error);
+      });
+  }, [date, activeEval, commit]);
+
+  return (
+    <>
+      <Header date={date} onDateChange={setDateAndUpdateUrl} />
+
+      <section className="section">
+        {summaryStatus === Status.Loading && (
+          <p>Downloading the summary graph...</p>
+        )}
+        {summaryStatus === Status.NotFound && date !== null && (
+          <p>No data found for date {date}</p>
+        )}
+        {summaryStatus === Status.NotFound &&
+          date === null &&
+          commit !== null && <p>No data found for commit {commit}</p>}
+        {summaryStatus === Status.NotFound &&
+          date === null &&
+          commit === null && <p>No data found</p>}
+        {summaryStatus === Status.Error && (
+          <p>Could not download the summary (error dumped in console)</p>
+        )}
+        {summaryStatus === Status.Ok && summary !== null && (
+          <SummaryViz
+            summary={summary as Summary}
+            activeEval={activeEval}
+            onActiveEvalChange={setActiveEval}
+          />
+        )}
+      </section>
+
+      <section className="section">
+        {evalStatsStatus === Status.Loading && activeEval !== null && (
+          <p>Downloading the eval stats...</p>
+        )}
+        {evalStatsStatus === Status.Error && activeEval !== null && (
+          <p>Could not download the eval stats (error dumped in console)</p>
+        )}
+        {evalStatsStatus === Status.Ok &&
+          activeEval !== null &&
+          activeEval !== "hello" &&
+          evalStats !== null && (
+            <EvalViz
+              activeEval={activeEval}
+              evalStats={evalStats as EvalStats}
+            />
+          )}
+      </section>
+    </>
+  );
+};
+
+export default App;
---
+++ js/website/src/components/Header.tsx
@@ -0,0 +1,72 @@
+import { dateString, randomColor } from "../utils";
+import Logo from "./Logo";
+
+const logoColors = [randomColor(), randomColor()];
+
+interface DatePickerProps {
+  date: string | null;
+  onDateChange: (date: string) => void;
+}
+
+const DatePicker = ({ date, onDateChange }: DatePickerProps) => {
+  const next = () => {
+    if (date === null) return;
+    const d = new Date(date);
+    d.setDate(d.getDate() + 1);
+    onDateChange(dateString(d));
+  };
+  const previus = () => {
+    if (date === null) return;
+    const d = new Date(date);
+    d.setDate(d.getDate() - 1);
+    onDateChange(dateString(d));
+  };
+
+  return (
+    <nav className="date-picker">
+      <button
+        className="date-picker__button"
+        disabled={date === null}
+        onClick={previus}
+      >
+        {" "}
+        ◀{" "}
+      </button>
+      <input
+        className="date-picker__input"
+        type="date"
+        value={date ?? ""}
+        onChange={(e) => onDateChange(e.target.value)}
+      />
+      <button
+        className="date-picker__button"
+        disabled={date === null}
+        onClick={next}
+      >
+        {" "}
+        ▶{" "}
+      </button>
+    </nav>
+  );
+};
+
+interface HeaderProps {
+  date: string | null;
+  onDateChange: (date: string) => void;
+}
+
+const Header = ({ date, onDateChange }: HeaderProps) => {
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <Logo colors={logoColors} />
+      </div>
+      <h1 className="header__title">
+        <a href="https://github.com/gradbench/gradbench">GradBench</a>
+      </h1>
+      <DatePicker date={date} onDateChange={onDateChange} />
+    </header>
+  );
+};
+
+export default Header;
