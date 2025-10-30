1e555f3245e96e5e0d55eda0b348b9af339e30ea
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2025-10-30 22:39:12 +0100

    Avoid OOM in Det.hs for horde-ad by using tensors instead of lists

--- tools/horde-ad/src/GradBench/Det.hs
+++ tools/horde-ad/src/GradBench/Det.hs
@@ -1,10 +1,9 @@
-{-# LANGUAGE OverloadedStrings #-}
--- | This is a somewhat inefficient implementation that directly
--- implements the recursive specification, using lists of lists to
--- represent the matrix. Remarkably, because the algorithm is
--- fundamentally so inefficient (something like O(n!)), the workloads
--- are necessarily tiny, and hence this inefficient representation is
--- adequate.
+{-# LANGUAGE OverloadedLists, OverloadedStrings #-}
+-- | This is an implementation that directly implements the recursive
+-- specification and so is fundamentally so inefficient (something like O(n!)). -- Fortunately, the workloads are necessarily tiny so this doesn't OOM.
+-- Due to the tiny workloads and, consequently, numerous but tiny tensors,
+-- the gradient but especially the primal are much slower than
+-- when implemented with lists for haskell-ad.
 module GradBench.Det
   ( Input,
     PrimalOutput,
@@ -16,7 +15,7 @@ where
 
 import Data.Aeson ((.:))
 import Data.Aeson qualified as JSON
-import Data.List qualified as L
+import Data.Array.Nested qualified as Nested
 import Data.Vector.Storable qualified as VS
 import HordeAd
 
@@ -33,38 +32,40 @@ instance JSON.FromJSON Input where
   parseJSON = JSON.withObject "input" $ \o ->
     Input <$> o .: "A" <*> o .: "ell"
 
-chunk :: Int -> [a] -> [[a]]
-chunk _ [] = []
-chunk n xs =
-  let (bef, aft) = splitAt n xs
-   in bef : chunk n aft
+chunk :: ADReady target
+      => Int -> VS.Vector Double -> target (TKR 2 Double)
+chunk n xs = rconcrete $ Nested.rfromVector [VS.length xs `div` n, n] xs
 
-picks :: [a] -> [[a]]
-picks l = zipWith (<>) (L.inits l) (tail $ L.tails l)
+picks :: ADReady target
+      => target (TKR 1 Double) -> target (TKR 2 Double)
+picks l = rgather @2 [rwidth l, rwidth l - 1] l
+                     (\ [n, n1] -> [ifH (n <=. n1) (n1 + 1) n1])
 
-parts :: [[a]] -> [[[a]]]
-parts = L.transpose . map picks . tail
+parts :: ADReady target
+      => target (TKR 2 Double) -> target (TKR 3 Double)
+parts m = rtr $ rbuild1 (rwidth m) (\i -> picks (m ! [i]))
 
-minors :: (Num a) => [[a]] -> [a]
-minors = map det . parts
+minors :: ADReady target
+       => target (TKR 2 Double) -> target (TKR 1 Double)
+minors m =
+  let parts_m = parts m
+  in rbuild1 (rwidth parts_m) (\i -> det (parts_m ! [i]))
 
-det :: (Num a) => [[a]] -> a
-det [[x]] = x
-det a = sum $ do
-  (f, aij, mij) <- zip3 (cycle [1, -1]) (head a) $ minors a
-  pure $ fromIntegral (f :: Int) * aij * mij
+det :: ADReady target
+    => target (TKR 2 Double) -> target (TKR 0 Double)
+det a | rsize a == 1 = a ! [0, 0]  -- needed to avoid "rbuild1: shape ambiguity"
+det a' = tlet a' $ \a ->
+  let minors_a = minors $ rslice 1 (rwidth a - 1) a
+      head_a = a ! [0]
+      cycle1 = ringestData [rwidth minors_a] $ take (rwidth minors_a)
+               $ cycle [1, -1]
+  in rsum0 $ cycle1 * head_a * minors_a
 
 primal :: Input -> PrimalOutput
-primal (Input a ell) = det $ chunk ell $ VS.toList a
+primal (Input a ell) = unConcrete $ kfromR . det $ chunk ell a
 
 gradient :: Input -> GradientOutput
 gradient (Input a ell) =
-  VS.fromList $ map unConcrete
-  $ cgrad (det . chunk ell) (map kconcrete $ VS.toList a)
-    -- Symbolic grad takes forever due to the build-up of product terms, because
-    -- lists are represented as nested products, and due to lack of explicit
-    -- sharing in the naively ported code. The former reason also makes
-    -- the non-symbolic cgrad >15 times slower than the haskell-ad
-    -- implementation, which we reuse here with minimal changes.
-    -- This variant also takes a lot of memory to allocate the products on tape
-    -- and so can't cope with size 11.
+  Nested.rtoVector . unConcrete $ grad (kfromR . det) (chunk ell a)
+    -- non-symbolic cgrad would take much more memory and time here
+    -- due to rbuild1 above
