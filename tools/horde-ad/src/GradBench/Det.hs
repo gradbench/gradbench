82040d0c8c43554bcba2eafb1c5363bc1d592d2a
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2025-11-01 20:03:26 +0100

    Change all remaining loops into horde-ad folds in Det.hs

--- tools/horde-ad/src/GradBench/Det.hs
+++ tools/horde-ad/src/GradBench/Det.hs
@@ -16,7 +16,6 @@ import Data.Array.Nested qualified as Nested
 import Data.Int (Int64)
 import Data.Vector.Storable qualified as VS
 import HordeAd
-import HordeAd.Core.Ops (ttletPlain)
 
 data Input = Input
   { _inputA :: VS.Vector Double,
@@ -40,15 +39,6 @@ fact n = factAcc 1 n
  where factAcc acc i | i <= 1 = acc
        factAcc acc i = factAcc (i * acc) (i - 1)
 
-updateR :: ADReady target
-        => Int
-        -> target (TKR 0 Int64) -> target (TKR 1 Int64)
-        -> target (TKR 1 Int64)
-updateR i a v' = tlet v' $ \v ->
-  rslice 0 i v
-  `rappend` rreplicate 1 a
-  `rappend` rslice (i + 1) (rwidth v - i - 1) v
-
 updateI :: ADReady target
         => PlainOf target (TKScalar Int64)
         -> target (TKR 0 Int64) -> target (TKR 1 Int64)
@@ -62,14 +52,16 @@ updateI idx a v =
 idx_to_perm :: forall target. ADReady target
             => Int -> PlainOf target (TKScalar Int64) -> target (TKR 1 Int64)
 idx_to_perm len idx0 =
-  let dummyEl = rscalar (-1)
+  let dummyEl :: forall f. ADReady f => f (TKR 0 Int64)
+      dummyEl = rscalar (-1)
       occupiedSpotMark :: forall f. ADReady f => f (TKR 0 Int64)
       occupiedSpotMark = rscalar (-1)
       perm0 = rreplicate len dummyEl
       elements0 = riota len
       fi0 = fact len
-  in tlet (tpair (rreplicate len dummyEl) 0) $ \acc0 ->
-    let allFreeSpots :: target (TKR 1 Int64) -> target (TKR 1 Int64)
+  in withSNat len $ \k ->
+    let allFreeSpots :: forall g. ADReady g
+                     => g (TKR 1 Int64) -> g (TKR 1 Int64)
         allFreeSpots elements1 =
           let f :: forall f. ADReady f
                 => f (TKProduct (TKR 1 Int64) (TKScalar Int64))
@@ -79,26 +71,44 @@ idx_to_perm len idx0 =
                 ifH (el ==. occupiedSpotMark)
                     acc
                     (tpair
-                       (updateI (tplainPart $ tproject2 acc) el (tproject1 acc))
+                       (updateI (kplainPart $ tproject2 acc) el (tproject1 acc))
                        (tproject2 acc + 1))
-          in withSNat len $ \k ->
-               tproject1 $ tfold k knownSTK knownSTK f acc0 elements1
-        loop :: target (TKR 1 Int64) -> target (TKR 1 Int64)
-             -> PlainOf target (TKScalar Int64) -> Int -> Int
-             -> target (TKR 1 Int64)
-        loop perm _ _ _ (-1) = perm
-        loop perm elements' idx' fi i =
-          let fi2 = fi `div` (i + 1)
-          in tlet elements' $ \elements ->
-             ttletPlain idx' $ \idx ->
-             tlet (allFreeSpots elements
-                   ! [idx `quotH` fromIntegral fi2]) $ \el ->
-               loop (updateR (len - i - 1) el perm)
-                    (updateI (tplainPart $ kfromR el) occupiedSpotMark elements)
-                    (idx `remH` fromIntegral fi2)
-                    fi2
-                    (i - 1)
-    in loop perm0 elements0 idx0 fi0 (len - 1)
+              acc0 = tpair (rreplicate len dummyEl) 0
+          in tproject1 $ tfold k knownSTK knownSTK f acc0 elements1
+        mkPerm :: target (TKR 1 Int64) -> target (TKR 1 Int64)
+               -> target (TKScalar Int64) -> target (TKScalar Int64)
+               -> target (TKR 1 Int64)
+        mkPerm perm1 elements1 idx1 fi1 =
+          let f :: forall f. ADReady f
+                => f (TKProduct (TKR 1 Int64)
+                                (TKProduct (TKR 1 Int64)
+                                           (TKProduct (TKScalar Int64)
+                                                      (TKScalar Int64))))
+                -> f (TKR 0 Int64)
+                -> f (TKProduct (TKR 1 Int64)
+                                (TKProduct (TKR 1 Int64)
+                                           (TKProduct (TKScalar Int64)
+                                                      (TKScalar Int64))))
+              f acc i =  -- sharing not needed, because these are variables
+                let perm = tproject1 acc
+                    elements' = tproject1 (tproject2 acc)
+                    idx' = tproject1 (tproject2 (tproject2 acc))
+                    fi' = tproject2 (tproject2 (tproject2 acc))
+                in tlet elements' $ \elements ->
+                   tlet idx' $ \idx ->
+                   tlet (fi' `quotH` (kfromR i + 1)) $ \fi2 ->
+                   tlet (allFreeSpots elements
+                         ! [kplainPart $ idx `quotH` fi2]) $ \el ->
+                     let perm2 = updateI (fromIntegral len
+                                          - kplainPart (kfromR i) - 1)
+                                         el perm
+                         elements2 = updateI (kplainPart $ kfromR el)
+                                             occupiedSpotMark elements
+                         idx2 = idx `remH` fi2
+                     in tpair perm2 (tpair elements2 (tpair idx2 fi2))
+              acc0 = tpair perm1 (tpair elements1 (tpair idx1 fi1))
+          in tproject1 $ tfold k knownSTK knownSTK f acc0 (rreverse $ riota len)
+    in mkPerm perm0 elements0 (kfromPlain idx0) (fromIntegral fi0)
 
 -- Compute the inversion number from a lexicographic index of a
 -- permutation.
@@ -108,18 +118,28 @@ idx_to_perm len idx0 =
 inversion_number_from_idx
   :: forall target. ADReady target
   => Int -> target (TKScalar Int64) -> target (TKScalar Int64)
-inversion_number_from_idx n idx0 =
-  let loop :: target (TKScalar Int64)
-           -> target (TKScalar Int64)
-           -> Int -> Int
-           -> target (TKScalar Int64)
-      loop s _ _ i | i == 0 = s
-      loop s idx' fi i = tlet idx' $ \idx ->
-        let fi2 = fi `div` (i + 1)
-            s2 = s + idx `quotH` fromIntegral fi2
-            idx2 = idx `remH` fromIntegral fi2
-        in loop s2 idx2 fi2 (i - 1)
-  in loop 0 idx0 (fact n) (n - 1)
+inversion_number_from_idx len idx0 =
+  let f :: forall f. ADReady f
+        => f (TKProduct (TKScalar Int64)
+                        (TKProduct (TKScalar Int64)
+                                   (TKScalar Int64)))
+        -> f (TKR 0 Int64)
+        -> f (TKProduct (TKScalar Int64)
+                        (TKProduct (TKScalar Int64)
+                                   (TKScalar Int64)))
+      f acc i =  -- sharing not needed, because these are variables
+        let s = tproject1 acc
+            idx' = tproject1 (tproject2 acc)
+            fi' = tproject2 (tproject2 acc)
+        in tlet idx' $ \idx ->
+           tlet (fi' `quotH` (kfromR i + 2)) $ \fi2 ->
+             let s2 = s + idx `quotH` fi2
+                 idx2 = idx `remH` fi2
+             in tpair s2 (tpair idx2 fi2)
+      acc0 = tpair 0 (tpair idx0 (fromIntegral $ fact len))
+  in withSNat (len - 1) $ \k1 ->
+       tproject1 $ tfold k1 knownSTK knownSTK f acc0
+                         (rreverse $ riota (len - 1))
 
 productR :: ADReady target
          => target (TKR 1 Double) -> target (TKScalar Double)
